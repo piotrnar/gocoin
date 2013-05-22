16ce94cdcc0b7c6e5a3ca434ac8d0712030b2dab
Piotr N <piotrnar@gmail.com>
2013-05-22 19:39:32 +0200

    Added support for openssl wrapper

--- tools/ecverbench.go
+++ tools/ecverbench.go
@@ -4,55 +4,13 @@ import (
 	"os"
 	"net"
 	"time"
-	"math/big"
 	"encoding/hex"
 	"github.com/piotrnar/gocoin/btc"
+	"github.com/piotrnar/gocoin/openssl"
 )
 
-
-func Verify2(kd []byte, sd []byte, hash []byte) bool {
-	pub, _ := btc.NewPublicKey(kd)
-	sig, _ := btc.NewSignature(sd)
-
-	// See [NSA] 3.4.2
-	c := pub.Curve
-	N := c.Params().N
-
-	e := new(big.Int).SetBytes(hash)
-	w := new(big.Int).ModInverse(sig.S, N)
-
-	u1 := e.Mul(e, w)
-	u1.Mod(u1, N)
-	u2 := w.Mul(sig.R, w)
-	u2.Mod(u2, N)
-
-	x1, y1 := c.ScalarBaseMult(u1.Bytes())
-	x2, y2 := c.ScalarMult(pub.X, pub.Y, u2.Bytes())
-	x, y := c.Add(x1, y1, x2, y2)
-	if x.Sign() == 0 && y.Sign() == 0 {
-		return false
-	}
-	x.Mod(x, N)
-	return x.Cmp(sig.R) == 0
-}
-
-
-func Verify(kd []byte, sd []byte, hash []byte) bool {
-	var res [1]byte
-	conn, e := net.DialTCP("tcp4", nil, &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: int(16667)})
-	if e != nil {
-		println(e.Error())
-		return false
-	}
-	conn.Write([]byte{1})
-	conn.Write([]byte{byte(len(kd))})
-	conn.Write(kd)
-	conn.Write([]byte{byte(len(sd))})
-	conn.Write(sd)
-	conn.Write(hash[0:32])
-	conn.Read(res[:])
-	conn.Close()
-	return res[0]!=0
+func EC_Verify(k, s, h []byte) bool {
+	return openssl.EC_Verify(k, s, h)==1
 }
 
 
@@ -61,8 +19,16 @@ const THREADS = 8
 
 func main() {
 	if len(os.Args)>1 {
-		btc.EcdsaServer = &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: int(16667)}
-		CNT *= 20
+		switch os.Args[1] {
+			case "tcp":
+				btc.EcdsaServer = &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: int(16667)}
+				CNT *= 20
+			case "openssl":
+				btc.EC_Verify = EC_Verify
+			default:
+				println("Specify either tcp or openssl as the only parameter")
+				return
+		}
 	}
 
 	key, _ := hex.DecodeString("040eaebcd1df2df853d66ce0e1b0fda07f67d1cabefde98514aad795b86a6ea66dbeb26b67d7a00e2447baeccc8a4cef7cd3cad67376ac1c5785aeebb4f6441c16")
