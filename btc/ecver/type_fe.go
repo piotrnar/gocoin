59fc54321fa54717d58c7d8779b918cf9baca08f
Piotr N <piotrnar@gmail.com>
2013-09-16 13:41:34 +0200

    Former-commit-id: 9e42f89b51589743e2d7056e8992199733a8858f

--- btc/ecver/type_fe.go
+++ btc/ecver/type_fe.go
@@ -10,10 +10,9 @@ type secp256k1_fe_t struct {
 	secp256k1_num_t
 }
 
-
-func new_fe_from_string(s string, base int) (res *secp256k1_fe_t) {
-	res = new(secp256k1_fe_t)
-	res.Int.SetString(s, base)
+func new_fe_from_string(s string, base int) (r *secp256k1_fe_t) {
+	r = new(secp256k1_fe_t)
+	r.Int.SetString(s, base)
 	return
 }
 
@@ -21,50 +20,73 @@ func (fe *secp256k1_fe_t) String() string {
 	return hex.EncodeToString(fe.Bytes())
 }
 
-
 func (a *secp256k1_fe_t) equal(b *secp256k1_fe_t) bool {
 	return a.Cmp(&b.Int)==0
 }
 
+func (a *secp256k1_fe_t) mul_s(b *secp256k1_fe_t) {
+	a.Mul(&a.Int, &b.Int)
+	a.Mod(&a.Int, secp256k1.P)
+	return
+}
 
-func (a *secp256k1_fe_t) mul(b *secp256k1_fe_t) (res *secp256k1_fe_t) {
-	res = new(secp256k1_fe_t)
-	res.Mul(&a.Int, &b.Int)
-	res.Mod(&res.Int, secp256k1.P)
+func (a *secp256k1_fe_t) mul_p(r *secp256k1_fe_t, b *secp256k1_fe_t) {
+	r.Mul(&a.Int, &b.Int)
+	r.Mod(&r.Int, secp256k1.P)
 	return
 }
 
 func (a *secp256k1_fe_t) mul_int(b int) {
-	t := new(big.Int).Set(&a.Int)
+	var ad big.Int
+	ad.Set(&a.Int)
 	for i:=1; i<b; i++ {
-		t.Add(t, &a.Int)
+		a.Add(&a.Int, &ad)
+		if a.Cmp(secp256k1.P) >= 0 {
+			a.Sub(&a.Int, secp256k1.P)
+		}
 	}
-	a.Mod(t, secp256k1.P)
 	return
 }
 
-
-func (a *secp256k1_fe_t) sqr() (res *secp256k1_fe_t) {
-	return a.mul(a)
+func (a *secp256k1_fe_t) sqr_p(r *secp256k1_fe_t) {
+	a.mul_p(r, a)
 }
 
+func (a *secp256k1_fe_t) sqr_s() {
+	a.mul_s(a)
+}
 
-func (a *secp256k1_fe_t) neg() (res *secp256k1_fe_t) {
-	res = new(secp256k1_fe_t)
-	res.Sub(secp256k1.P, &a.Int)
+func (a *secp256k1_fe_t) neg_s() {
+	a.Sub(secp256k1.P, &a.Int)
 	return
 }
 
-func (a *secp256k1_fe_t) add(b *secp256k1_fe_t) (res *secp256k1_fe_t) {
-	res = new(secp256k1_fe_t)
-	res.Add(&a.Int, &b.Int)
-	res.Mod(&res.Int, secp256k1.P)
+func (a *secp256k1_fe_t) neg_p(r *secp256k1_fe_t) {
+	r.Sub(secp256k1.P, &a.Int)
 	return
 }
 
+func (a *secp256k1_fe_t) add_p(r *secp256k1_fe_t, b *secp256k1_fe_t) {
+	r.Add(&a.Int, &b.Int)
+	if r.Cmp(secp256k1.P) >= 0 {
+		r.Sub(&r.Int, secp256k1.P)
+	}
+	return
+}
 
-func (a *secp256k1_fe_t) inv() (r *secp256k1_fe_t) {
-	r = new(secp256k1_fe_t)
+func (a *secp256k1_fe_t) add_s(b *secp256k1_fe_t) {
+	a.Add(&a.Int, &b.Int)
+	if a.Cmp(secp256k1.P) >= 0 {
+		a.Sub(&a.Int, secp256k1.P)
+	}
+	return
+}
+
+func (a *secp256k1_fe_t) inv_p(r *secp256k1_fe_t) {
 	r.ModInverse(&a.Int, secp256k1.P)
 	return
+}
+
+func (a *secp256k1_fe_t) inv_s() {
+	a.ModInverse(&a.Int, secp256k1.P)
 }
