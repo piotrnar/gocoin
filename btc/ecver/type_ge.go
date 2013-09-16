2c3ef6bdd4c4bded8ee37461dfd578fb1cd9a13c
Piotr N <piotrnar@gmail.com>
2013-09-16 13:41:36 +0200

    Former-commit-id: 5e786ff57a582d49d5f9fd95d43fb6e655b79249

--- btc/ecver/type_ge.go
+++ btc/ecver/type_ge.go
@@ -12,11 +12,12 @@ type secp256k1_ge_t struct {
 }
 
 func (r *secp256k1_ge_t) set_gej(a *secp256k1_gej_t) {
-	a.z = *a.z.inv()
-	z2 := a.z.sqr()
-	z3 := a.z.mul(z2)
-	a.x = *a.x.mul(z2)
-	a.y = *a.y.mul(z3)
+	var z2, z3 secp256k1_fe_t
+	a.z.inv_s()
+	a.z.sqr_p(&z2)
+	a.z.mul_p(&z3, &z2)
+	a.x.mul_s(&z2)
+	a.y.mul_s(&z3)
 	a.z.Set(BigInt1)
 	r.infinity = a.infinity
 	r.x = a.x
@@ -28,22 +29,24 @@ func (ge *secp256k1_ge_t) print(lab string) {
 	println("GE." + lab + ".y:", hex.EncodeToString(ge.y.Bytes()))
 }
 
-func (a *secp256k1_ge_t) neg() (r *secp256k1_ge_t) {
-	r = new(secp256k1_ge_t)
+func (a *secp256k1_ge_t) neg_p(rr *secp256k1_ge_t) {
+	var r secp256k1_ge_t
 	r.infinity = a.infinity
 	r.x = a.x
-	r.y = *a.y.neg()
+	a.y.neg_p(&r.y)
+	*rr = r
 	return
 }
 
 func (a *secp256k1_ge_t) precomp(w int) (pre []secp256k1_ge_t) {
 	pre = make([]secp256k1_ge_t, (1 << (uint(w)-2)))
 	pre[0] = *a;
-	var x secp256k1_gej_t
+	var x, d, tmp secp256k1_gej_t
 	x.set_ge(a)
-	d := x.double()
+	x.double_p(&d)
 	for i:=1 ; i<len(pre); i++ {
-		pre[i].set_gej(d.add_ge(&pre[i-1]))
+		d.add_ge_p(&tmp, &pre[i-1])
+		pre[i].set_gej(&tmp)
 	}
 	return
 }
