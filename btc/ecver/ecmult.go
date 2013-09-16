915fb6335a33bf07983f2eca149e5b549f028d28
Piotr N <piotrnar@gmail.com>
2013-09-16 13:41:29 +0200

    Former-commit-id: 8c4ee0f394978633426449d8900c5ee865580ffb

--- btc/ecver/ecmult.go
+++ btc/ecver/ecmult.go
@@ -16,37 +16,37 @@ func secp256k1_ecmult_start() {
 	g.y.Set(secp256k1.Gy)
 
 	// calculate 2^128*generator
-	g_128j := new(secp256k1_gej_t)
+	var g_128j secp256k1_gej_t
 	g_128j.set_ge(&g)
 
 	for i := 0; i < 128; i++ {
-		g_128j = g_128j.double()
+		g_128j.double_p(&g_128j)
 	}
 
 	var g_128 secp256k1_ge_t
-	g_128.set_gej(g_128j)
+	g_128.set_gej(&g_128j)
 
     // precompute the tables with odd multiples
 	pre_g = g.precomp(WINDOW_G)
 	pre_g_128 = g_128.precomp(WINDOW_G)
 
 	// compute prec and fin
-	gg := new(secp256k1_gej_t)
+	var gg secp256k1_gej_t
 	gg.set_ge(&g)
 	ad := g
-	fn := new(secp256k1_gej_t)
+	var fn secp256k1_gej_t
 	fn.infinity = true
 	for j:=0; j<64; j++ {
-		prec[j][0].set_gej(gg)
-		fn = fn.add(gg)
+		prec[j][0].set_gej(&gg)
+		fn.add_p(&fn, &gg)
 		for i:=1; i<16; i++ {
-			gg = gg.add_ge(&ad)
-			prec[j][i].set_gej(gg)
+			gg.add_ge_p(&gg, &ad)
+			prec[j][i].set_gej(&gg)
 		}
 		ad = prec[j][15]
 	}
-	fin.set_gej(fn)
-	fin = *fin.neg()
+	fin.set_gej(&fn)
+	fin.neg_p(&fin)
 }
 
 
@@ -78,7 +78,7 @@ func secp256k1_ecmult_wnaf(wnaf []int, a *secp256k1_num_t, w uint) (ret int) {
 	return
 }
 
-
+var dbg bool
 
 func secp256k1_ecmult(a *secp256k1_gej_t, na, ng *secp256k1_num_t) (r *secp256k1_gej_t) {
 	r = new(secp256k1_gej_t)
@@ -98,7 +98,8 @@ func secp256k1_ecmult(a *secp256k1_gej_t, na, ng *secp256k1_num_t) (r *secp256k1
 	bits_ng_128 := secp256k1_ecmult_wnaf(wnaf_ng_128[:], ng_128, WINDOW_G)
 
 	// calculate a_lam = a*lambda
-	a_lam := a.mul_lambda()
+	a_lam := *a
+	a_lam.mul_lambda_s()
 
 	// calculate odd multiples of a and a_lam
 	pre_a_1 := a.precomp(WINDOW_A)
@@ -122,13 +123,13 @@ func secp256k1_ecmult(a *secp256k1_gej_t, na, ng *secp256k1_num_t) (r *secp256k1
 	var n int
 
 	for i:=bits-1; i>=0; i-- {
-		r = r.double()
+		r.double_p(r)
 
 		if i < bits_na_1 {
 			n = wnaf_na_1[i]
 			if n!=0 {
 				ECMULT_TABLE_GET_GEJ(&tmpj, pre_a_1, n, WINDOW_A)
-				r = r.add(&tmpj)
+				r.add_p(r, &tmpj)
 			}
 		}
 
@@ -136,7 +137,7 @@ func secp256k1_ecmult(a *secp256k1_gej_t, na, ng *secp256k1_num_t) (r *secp256k1
 			n = wnaf_na_lam[i]
 			if n!=0 {
 				ECMULT_TABLE_GET_GEJ(&tmpj, pre_a_lam, n, WINDOW_A)
-				r = r.add(&tmpj)
+				r.add_p(r, &tmpj)
 			}
 		}
 
@@ -144,7 +145,7 @@ func secp256k1_ecmult(a *secp256k1_gej_t, na, ng *secp256k1_num_t) (r *secp256k1
 			n = wnaf_ng_1[i]
 			if n!=0 {
 				ECMULT_TABLE_GET_GE(&tmpa, pre_g, n, WINDOW_G)
-				r = r.add_ge(&tmpa)
+				r.add_ge_p(r, &tmpa)
 			}
 		}
 
@@ -152,7 +153,7 @@ func secp256k1_ecmult(a *secp256k1_gej_t, na, ng *secp256k1_num_t) (r *secp256k1
 			n = wnaf_ng_128[i]
 			if n!=0 {
 				ECMULT_TABLE_GET_GE(&tmpa, pre_g_128, n, WINDOW_G);
-				r = r.add_ge(&tmpa)
+				r.add_ge_p(r, &tmpa)
 			}
 		}
 	}
@@ -165,7 +166,7 @@ func ECMULT_TABLE_GET_GEJ(r *secp256k1_gej_t, pre []secp256k1_gej_t, n int, w ui
 	if n > 0 {
 		*r = pre[((n)-1)/2]
 	} else {
-		*r = *pre[(-(n)-1)/2].neg()
+		pre[(-(n)-1)/2].neg_p(r)
 	}
 }
 
@@ -173,6 +174,6 @@ func ECMULT_TABLE_GET_GE(r *secp256k1_ge_t, pre []secp256k1_ge_t, n int, w uint)
 	if n > 0 {
 		*r = pre[((n)-1)/2]
 	} else {
-		*r = *pre[(-(n)-1)/2].neg()
+		pre[(-(n)-1)/2].neg_p(r)
 	}
 }
