54f67b663a78e198336d111efc04284e94feb7de
Piotr N <piotrnar@gmail.com>
2013-09-19 22:45:24 +0200

    Former-commit-id: 5921227364643d8b938ae50d38b444153fa4be16

--- btc/newec/typ_gej_cgo.go
+++ btc/newec/typ_gej_cgo.go
@@ -14,13 +14,6 @@ import (
 	"unsafe"
 )
 
-func (a *gej_t) _ecmult(r *gej_t, na, ng *num_t) {
-	println("gej.ecmult not implemented")
-	na_bn := na.get_bn()
-	ng_bn := ng.get_bn()
-	C.secp256k1_ecmult(unsafe.Pointer(&r.x), unsafe.Pointer(&a.x), na_bn, ng_bn)
-}
-
 func (a *gej_t) double(r *gej_t) {
 	C.secp256k1_gej_double(unsafe.Pointer(r), unsafe.Pointer(a))
 }
@@ -37,9 +30,3 @@ func (a *gej_t) mul_lambda(r *gej_t) {
 	C.secp256k1_gej_mul_lambda(unsafe.Pointer(r), unsafe.Pointer(a))
 }
 
-
-/*
-func (a *gej_t) get_x(r *fe_t) {
-	C.secp256k1_gej_get_x(unsafe.Pointer(r), unsafe.Pointer(&a.x))
-}
-*/
