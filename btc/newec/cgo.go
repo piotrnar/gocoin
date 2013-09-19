65ae8e5b17893291885c0342063246ed847a277f
Piotr N <piotrnar@gmail.com>
2013-09-19 23:19:13 +0200

    Former-commit-id: 153c6252601e2d9c23b3c0e8ca4696c954bfee7e

--- btc/newec/cgo.go
+++ btc/newec/cgo.go
@@ -7,19 +7,12 @@ package newec
 
 #include "secp256k1.h"
 
-void secp256k1_fe_sqrt(void *r, void *a);
 void secp256k1_gej_double(void *r, void *a);
 void secp256k1_gej_add_ge(void *r, void *a, void *b);
 void secp256k1_gej_add(void *r, void *a, void *b);
 */
 import "C"
 import "unsafe"
-
-/*
-func (a *fe_t) sqrt(r *fe_t) {
-	C.secp256k1_fe_sqrt(unsafe.Pointer(r), unsafe.Pointer(a));
-}
-*/
 
 func (a *gej_t) double(r *gej_t) {
 	C.secp256k1_gej_double(unsafe.Pointer(r), unsafe.Pointer(a))
