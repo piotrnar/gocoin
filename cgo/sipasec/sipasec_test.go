4ac150006c20dfba4a5347eb29d44f466906cdef
Piotr N <piotrnar@gmail.com>
2013-09-20 15:31:26 +0200

    Former-commit-id: 0cf1549f3e622e02525a8303612f477419ae7428

--- cgo/sipasec/sipasec_test.go
+++ cgo/sipasec/sipasec_test.go
@@ -78,3 +78,24 @@ func TestVerify1(t *testing.T) {
 	}
 
 }
+
+func BenchmarkVerifyUncompressed(b *testing.B) {
+	key, _ := hex.DecodeString("040eaebcd1df2df853d66ce0e1b0fda07f67d1cabefde98514aad795b86a6ea66dbeb26b67d7a00e2447baeccc8a4cef7cd3cad67376ac1c5785aeebb4f6441c16")
+	sig, _ := hex.DecodeString("3045022100fe00e013c244062847045ae7eb73b03fca583e9aa5dbd030a8fd1c6dfcf11b1002207d0d04fed8fa1e93007468d5a9e134b0a7023b6d31db4e50942d43a250f4d07c01")
+	msg, _ := hex.DecodeString("3382219555ddbb5b00e0090f469e590ba1eae03c7f28ab937de330aa60294ed6")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		EC_Verify(key, sig, msg)
+	}
+}
+
+func BenchmarkVerifyCompressed(b *testing.B) {
+	key_compr, _ := hex.DecodeString("020eaebcd1df2df853d66ce0e1b0fda07f67d1cabefde98514aad795b86a6ea66d")
+	sig, _ := hex.DecodeString("3045022100fe00e013c244062847045ae7eb73b03fca583e9aa5dbd030a8fd1c6dfcf11b1002207d0d04fed8fa1e93007468d5a9e134b0a7023b6d31db4e50942d43a250f4d07c01")
+	msg, _ := hex.DecodeString("3382219555ddbb5b00e0090f469e590ba1eae03c7f28ab937de330aa60294ed6")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		EC_Verify(key_compr, sig, msg)
+	}
+}
+
