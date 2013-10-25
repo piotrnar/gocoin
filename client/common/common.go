0d21db40e9ba54e54a4ed318464edc0b95b8d9ca
Piotr N <piotrnar@gmail.com>
2013-10-25 20:03:49 +0200

    Former-commit-id: 0b64d91d55ecc65fd66f99f70f3c7d3619d1c46e

---
+++ client/common/common.go
@@ -0,0 +1,103 @@
+package common
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	"github.com/piotrnar/gocoin/btc"
+)
+
+const (
+	ConfigFile = "gocoin.conf"
+
+	Version = 70001
+	UserAgent = "/Gocoin:"+btc.SourcesTag+"/"
+	Services = uint64(0x00000001)
+
+	MaxCachedBlocks = 600
+)
+
+var (
+	BlockChain *btc.Chain
+	GenesisBlock *btc.Uint256
+	Magic [4]byte
+	AddrVersion byte
+
+	Last struct {
+		sync.Mutex // use it for writing and reading from non-chain thread
+		Block *btc.BlockTreeNode
+		time.Time
+	}
+
+	GocoinHomeDir string
+	StartTime time.Time
+	MaxPeersNeeded int
+
+	DefaultTcpPort uint16
+
+	MaxExpireTime time.Duration
+	ExpirePerKB time.Duration
+
+	DebugLevel int64
+
+	CounterMutex sync.Mutex
+	Counter map[string] uint64 = make(map[string]uint64)
+
+	BusyWith string
+	Busy_mutex sync.Mutex
+)
+
+
+func CountSafe(k string) {
+	CounterMutex.Lock()
+	Counter[k]++
+	CounterMutex.Unlock()
+}
+
+func CountSafeAdd(k string, val uint64) {
+	CounterMutex.Lock()
+	Counter[k] += val
+	CounterMutex.Unlock()
+}
+
+
+func Busy(b string) {
+	Busy_mutex.Lock()
+	BusyWith = b
+	Busy_mutex.Unlock()
+}
+
+
+func BytesToString(val uint64) string {
+	if val < 1e6 {
+		return fmt.Sprintf("%.1f KB", float64(val)/1e3)
+	} else if val < 1e9 {
+		return fmt.Sprintf("%.2f MB", float64(val)/1e6)
+	}
+	return fmt.Sprintf("%.2f GB", float64(val)/1e9)
+}
+
+
+func NumberToString(num float64) string {
+	if num>1e15 {
+		return fmt.Sprintf("%.2f P", num/1e15)
+	}
+	if num>1e12 {
+		return fmt.Sprintf("%.2f T", num/1e12)
+	}
+	if num>1e9 {
+		return fmt.Sprintf("%.2f G", num/1e9)
+	}
+	if num>1e6 {
+		return fmt.Sprintf("%.2f M", num/1e6)
+	}
+	if num>1e3 {
+		return fmt.Sprintf("%.2f K", num/1e3)
+	}
+	return fmt.Sprintf("%.2f", num)
+}
+
+
+func HashrateToString(hr float64) string {
+	return NumberToString(hr)+"H/s"
+}
