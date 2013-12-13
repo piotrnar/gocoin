266e490171aa0954239a97d93774a291e67fbfa5
Piotr N <piotrnar@gmail.com>
2013-12-13 10:01:47 +0100

    Former-commit-id: ad5134262e21c416d9e17ef9f05de70988e98db7

--- wallet/config.go
+++ wallet/config.go
@@ -75,13 +75,7 @@ func parse_config() {
 					println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
 				}
 
-			case "secrand":
-				v, e := strconv.ParseBool(ll[1])
-				if e == nil {
-					*secrand = v
-				} else {
-					println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
-				}
+			// case "secrand": <-- deprecated
 
 			case "fee":
 				v, e := strconv.ParseFloat(ll[1], 64)
