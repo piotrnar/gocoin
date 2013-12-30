f7300efbace6192e395bb16a2260f97b0022bcfc
Piotr N <piotrnar@gmail.com>
2013-12-30 12:10:24 +0100

    Former-commit-id: 026db67c0197d97f9256368c73c9e6787327ebed

--- btc/block.go
+++ btc/block.go
@@ -15,6 +15,8 @@ type Block struct {
 	MerkleRoot []byte
 	BlockTime uint32
 	Bits uint32
+	Nonce uint32
+	TxCount, TxOffset int  // Number of transactions and byte offset to the first one
 
 	// if the block is trusted, we do not check signatures and some other things...
 	Trusted bool
@@ -34,6 +36,12 @@ func NewBlock(data []byte) (*Block, error) {
 	bl.MerkleRoot = data[36:68]
 	bl.BlockTime = binary.LittleEndian.Uint32(data[68:72])
 	bl.Bits = binary.LittleEndian.Uint32(data[72:76])
+	bl.Nonce = binary.LittleEndian.Uint32(data[76:80])
+	bl.TxCount, bl.TxOffset = VLen(data[80:])
+	if bl.TxOffset == 0 {
+		return nil, errors.New("Block's txn_count field corrupt")
+	}
+	bl.TxOffset += 80
 	return &bl, nil
 }
 
@@ -41,20 +49,17 @@ func NewBlock(data []byte) (*Block, error) {
 // Parses block's transactions and adds them to the structure, calculating hashes BTW.
 // It would be more elegant to use bytes.Reader here, but this solution is ~20% faster.
 func (bl *Block) BuildTxList() (e error) {
-	offs := int(80)
-	txcnt, n := VLen(bl.Raw[offs:])
-	if n == 0 {
-		e = errors.New("Unexpected end of the block's payload")
-	}
-	offs += n
-	bl.Txs = make([]*Tx, txcnt)
+	bl.Txs = make([]*Tx, bl.TxCount)
+
+	offs := bl.TxOffset
 
 	done := make(chan bool, UseThreads)
 	for i:=0; i<UseThreads; i++ {
 		done <- false
 	}
 
-	for i:=0; i<int(txcnt); i++ {
+	for i:=0; i<bl.TxCount; i++ {
+		var n int
 		bl.Txs[i], n = NewTx(bl.Raw[offs:])
 		if bl.Txs[i] == nil || n==0 {
 			e = errors.New("NewTx failed")
@@ -63,13 +68,13 @@ func (bl *Block) BuildTxList() (e error) {
 		bl.Txs[i].Size = uint32(n)
 		_ = <- done // wait here, if we have too many threads already
 		go func(h **Uint256, b []byte) {
-			*h = NewSha2Hash(b)
+			*h = NewSha2Hash(b) // Calculate tx hash in a background
 			done <- true // indicate mission completed
 		}(&bl.Txs[i].Hash, bl.Raw[offs:offs+n])
 		offs += n
 	}
 
-	// Wait for all the pending mission to complete...
+	// Wait for all the pending missions to complete...
 	for i:=0; i<UseThreads; i++ {
 		_ = <- done
 	}
