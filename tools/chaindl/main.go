fed1e6708dfe1291824854091b5e171466fd82ad
Piotr N <piotrnar@gmail.com>
2014-01-16 21:48:15 +0100

    Former-commit-id: d5a895b3ab08afec0078db9b34c32c4970a53141

--- tools/chaindl/main.go
+++ tools/chaindl/main.go
@@ -40,7 +40,7 @@ func main() {
 	GenesisBlock = btc.NewUint256FromString("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
 	Magic = [4]byte{0xF9,0xBE,0xB4,0xD9}
 	if len(os.Args)<2 {
-		GocoinHomeDir = utils.BitcoinHome()
+		GocoinHomeDir = utils.BitcoinHome() + "gocoin" + string(os.PathSeparator)
 	} else {
 		GocoinHomeDir = os.Args[1]
 		if GocoinHomeDir[0]!=os.PathSeparator {
