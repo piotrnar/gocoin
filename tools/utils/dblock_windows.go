658fa862f1cc306d2c95fd07fd71c24af9dd3996
Piotr N <piotrnar@gmail.com>
2014-01-16 19:57:18 +0100

    Former-commit-id: add3898be3d6dd738a154169b57ecf32bc76147c

---
+++ tools/utils/dblock_windows.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"os"
+)
+
+var (
+	DbLockFileName string
+	DbLockFileHndl *os.File
+)
+
+func LockDatabaseDir(GocoinHomeDir string) {
+	var e error
+	DbLockFileName = GocoinHomeDir+".lock"
+	os.Remove(DbLockFileName)
+	DbLockFileHndl, e = os.OpenFile(DbLockFileName, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0660)
+	if e != nil {
+		println(e.Error())
+		println("Could not lock the databse folder for writing. Another instance might be running.")
+		println("Make sure you can delete and recreate file:", DbLockFileName)
+		os.Exit(1)
+	}
+}
+
+func UnlockDatabaseDir() {
+	DbLockFileHndl.Close()
+	os.Remove(DbLockFileName)
+}
