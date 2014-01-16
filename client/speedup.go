bd59c97db7298bcbd635c8522046aba705683a3c
Piotr N <piotrnar@gmail.com>
2014-01-16 21:14:25 +0100

    Former-commit-id: 6d91851a43f1856dfb3b128246a09fd1dd77a0b2

--- client/speedup.go
+++ client/speedup.go
@@ -4,15 +4,10 @@ import (
 	"fmt"
 	"github.com/piotrnar/gocoin/btc"
 	"github.com/piotrnar/gocoin/btc/newec"
-	"github.com/piotrnar/gocoin/client/common"
 )
 
 func EC_Verify(k, s, h []byte) bool {
-	res := newec.Verify(k, s, h)
-	if !res {
-		common.CountSafe("ECVerifyFail")
-	}
-	return res
+	return newec.Verify(k, s, h)
 }
 
 func init() {
