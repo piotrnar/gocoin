c3de41adaa1fcb258a692bcf3a6b8eb18850db99
Piotr N <piotrnar@gmail.com>
2014-01-16 19:27:03 +0100

    Former-commit-id: 8094d260935e4309f422583fd77893d9ccaac7d0

---
+++ tools/chaindl/mutex.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"runtime"
+)
+
+var (
+	BlockMutexFile string
+	BlockMutexLine int
+)
+
+func BlocksMutex_Lock() {
+	BlocksMutex.Lock()
+	_, BlockMutexFile, BlockMutexLine, _ = runtime.Caller(1)
+}
+
+func BlocksMutex_Unlock() {
+	_, f, l, _ := runtime.Caller(1)
+	BlockMutexFile = f+"-done"
+	BlockMutexLine = l
+	BlocksMutex.Unlock()
+}
