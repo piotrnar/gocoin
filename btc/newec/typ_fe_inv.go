6b52af05449d98ab6cb9df988141de4f25039514
Piotr N <piotrnar@gmail.com>
2014-02-08 11:07:47 +0100

    Former-commit-id: 9bd7eafc8fa44b73aae80f0af65114146cb09c61

--- btc/newec/typ_fe_inv.go
+++ btc/newec/typ_fe_inv.go
@@ -6,6 +6,8 @@ import (
 // New algo by peterdettman - https://github.com/sipa/secp256k1/pull/19
 func (a *fe_t) inv(r *fe_t) {
 	var x2, x3, x6, x9, x11, x22, x44, x88, x176, x220, x223, t1 fe_t
+	var j int
+
 	a.sqr(&x2)
 	x2.mul(&x2, a)
 
@@ -27,31 +29,31 @@ func (a *fe_t) inv(r *fe_t) {
 	x11.mul(&x11, &x2)
 
 	x11.sqr(&x22)
-	for j:=1; j<11; j++ {
+	for j=1; j<11; j++ {
 		x22.sqr(&x22)
 	}
 	x22.mul(&x22, &x11)
 
 	x22.sqr(&x44)
-	for j:=1; j<22; j++ {
+	for j=1; j<22; j++ {
 		x44.sqr(&x44)
 	}
 	x44.mul(&x44, &x22)
 
 	x44.sqr(&x88)
-	for j:=1; j<44; j++ {
+	for j=1; j<44; j++ {
 		x88.sqr(&x88)
 	}
 	x88.mul(&x88, &x44)
 
 	x88.sqr(&x176)
-	for j:=1; j<88; j++ {
+	for j=1; j<88; j++ {
 		x176.sqr(&x176)
 	}
 	x176.mul(&x176, &x88)
 
 	x176.sqr(&x220)
-	for j:=1; j<44; j++ {
+	for j=1; j<44; j++ {
 		x220.sqr(&x220)
 	}
 	x220.mul(&x220, &x44)
@@ -63,7 +65,7 @@ func (a *fe_t) inv(r *fe_t) {
 
 
 	x223.sqr(&t1)
-	for j:=1; j<23; j++ {
+	for j=1; j<23; j++ {
 		t1.sqr(&t1)
 	}
 	t1.mul(&t1, &x22)
