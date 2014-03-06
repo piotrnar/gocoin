cc2b4ed40040543256bea24b29d73389fee31cb2
Piotr N <piotrnar@gmail.com>
2014-03-06 13:10:07 +0100

    Former-commit-id: 682eca5d92cd925d6db9a78e4db2d7a1ac76766f

--- wallet/main.go
+++ wallet/main.go
@@ -18,6 +18,7 @@ var (
 
 	// Wallet options
 	dump *bool = flag.Bool("l", false, "List public addressses from the wallet")
+	singleask *bool = flag.Bool("1", false, "Do not re-ask for the password (when used along with -l)")
 	noverify *bool = flag.Bool("q", false, "Do not verify keys while listing them")
 	keycnt *uint = flag.Uint("n", 50, "Set the number of keys to be used")
 	uncompressed *bool = flag.Bool("u", false, "Use uncompressed public keys")
