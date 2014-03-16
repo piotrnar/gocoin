e473d4bac242b29902e44ee26098af5e6b7bbdaf
Piotr N <piotrnar@gmail.com>
2014-03-16 10:05:02 +0100

    Former-commit-id: 45b8727a72c17c673322631f230a4fd4bf139be2

---
+++ client/wallet/multisig.go
@@ -0,0 +1,44 @@
+package wallet
+
+import (
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/piotrnar/gocoin/btc"
+	"github.com/piotrnar/gocoin/client/common"
+)
+
+type MultisigAddr struct {
+	MultiAddress string
+	ScriptPubKey string
+	KeysRequired, KeysProvided uint
+	RedeemScript string
+	ListOfAddres []string
+}
+
+func IsMultisig(ad *btc.BtcAddr) (yes bool, rec *MultisigAddr) {
+	yes = ad.Version==btc.AddrVerScript(common.Testnet)
+	if !yes {
+		return
+	}
+
+	fn := common.GocoinHomeDir + "wallet" +
+		string(os.PathSeparator) + "multisig" +
+		string(os.PathSeparator) + ad.String() + ".json"
+
+	d, er := ioutil.ReadFile(fn)
+	if er != nil {
+		//println("fn", fn, er.Error())
+		return
+	}
+
+	var msa MultisigAddr
+	er = json.Unmarshal(d, &msa)
+	if er == nil {
+		rec = &msa
+	} else {
+		println(fn, er.Error())
+	}
+
+	return
+}
