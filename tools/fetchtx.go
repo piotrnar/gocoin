842455967fcfcfacab2b6b4f2c77cbdd3f1c88fe
Piotr N <piotrnar@gmail.com>
2014-04-02 12:39:30 +0200

    Former-commit-id: aa934f0f35c553b3e299486d776389097efc4044

--- tools/fetchtx.go
+++ tools/fetchtx.go
@@ -18,12 +18,9 @@ func main() {
 	}
 
 	txid := btc.NewUint256FromString(os.Args[1])
-	rawtx, brokentx := utils.GetTxFromExplorer(txid)
+	rawtx := utils.GetTxFromWeb(txid)
 	if rawtx==nil {
 		fmt.Println("Error fetching the transaction")
-		if brokentx!=nil {
-			ioutil.WriteFile("bad-"+txid.String()+".tx", brokentx, 0666)
-		}
 	} else {
 		ioutil.WriteFile(txid.String()+".tx", rawtx, 0666)
 	}
