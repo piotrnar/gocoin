6e9cd91e327f869af5769455e20906ff03d4fdc8
Piotr N <piotrnar@gmail.com>
2014-04-13 17:45:22 +0200

    Former-commit-id: ad77e033c60cac1de52301acc8072948b3b8528b

--- client/common/config.go
+++ client/common/config.go
@@ -64,7 +64,7 @@ var (
 		}
 		Beeps struct {
 			NewBlock bool  // beep when a new block has been mined
-			ActiveFork bool  // triple beep when ther is a fork
+			ActiveFork bool  // triple beep when there is a fork
 			NewBalance bool // been when a balance has changed
 			MinerID string // beep when a bew block is mined with this string in coinbase
 		}
