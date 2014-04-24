d1e06090bf9fd888d52d6bb3a446c536f5905afc
Piotr N <piotrnar@gmail.com>
2014-04-24 11:31:12 +0200

    Former-commit-id: c1b43d6030d17e8f1b1acbcd0bc8cd243d2a638c

--- client/usif/webui/config.go
+++ client/usif/webui/config.go
@@ -21,31 +21,74 @@ func p_cfg(w http.ResponseWriter, r *http.Request) {
 	common.LockCfg()
 	defer common.UnlockCfg()
 
-	if checksid(r) && len(r.Form["txponoff"])>0 {
+	if r.Method=="POST" {
+		if len(r.Form["configjson"])>0 {
+			e := json.Unmarshal([]byte(r.Form["configjson"][0]), &common.CFG)
+			if e == nil {
+				common.Reset()
+			}
+			if len(r.Form["save"])>0 {
+				dat, _ := json.Marshal(&common.CFG)
+				if dat != nil {
+					ioutil.WriteFile(common.ConfigFile, dat, 0660)
+				}
+			}
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+
+		if len(r.Form["walletdata"])>0 && len(r.Form["walletfname"])>0 {
+			fn := r.Form["walletfname"][0]
+			if fn=="" {
+				fn = wallet.DefaultFileName
+			}
+			fn = common.GocoinHomeDir + "wallet" + string(os.PathSeparator) + fn
+			ioutil.WriteFile(fn, []byte(r.Form["walletdata"][0]), 0660)
+			wallet.LoadWallet(fn)
+			http.Redirect(w, r, "/wal", http.StatusFound)
+			return
+		}
+
+		if len(r.Form["shutdown"])>0 {
+			usif.Exit_now = true
+			w.Write([]byte("Your node should shut down soon"))
+			return
+		}
+
+		return
+	}
+
+	// for any other GET we need a matching session-id
+	if !checksid(r) {
+		new_session_id(w)
+		return
+	}
+
+	if len(r.Form["txponoff"])>0 {
 		common.CFG.TXPool.Enabled = !common.CFG.TXPool.Enabled
 		http.Redirect(w, r, "txs", http.StatusFound)
 		return
 	}
 
-	if checksid(r) && len(r.Form["txronoff"])>0 {
+	if len(r.Form["txronoff"])>0 {
 		common.CFG.TXRoute.Enabled = !common.CFG.TXRoute.Enabled
 		http.Redirect(w, r, "txs", http.StatusFound)
 		return
 	}
 
-	if checksid(r) && len(r.Form["lonoff"])>0 {
+	if len(r.Form["lonoff"])>0 {
 		common.CFG.Net.ListenTCP = !common.CFG.Net.ListenTCP
 		http.Redirect(w, r, "net", http.StatusFound)
 		return
 	}
 
-	if checksid(r) && len(r.Form["drop"])>0 {
+	if len(r.Form["drop"])>0 {
 		network.DropPeer(r.Form["drop"][0])
 		http.Redirect(w, r, "net", http.StatusFound)
 		return
 	}
 
-	if checksid(r) && len(r.Form["savecfg"])>0 {
+	if len(r.Form["savecfg"])>0 {
 		dat, _ := json.Marshal(&common.CFG)
 		if dat != nil {
 			ioutil.WriteFile(common.ConfigFile, dat, 0660)
@@ -54,52 +97,19 @@ func p_cfg(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if checksid(r) && len(r.Form["beepblock"])>0 {
+	if len(r.Form["beepblock"])>0 {
 		common.CFG.Beeps.NewBlock = !common.CFG.Beeps.NewBlock
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
 
-	if checksid(r) && len(r.Form["freemem"])>0 {
+	if len(r.Form["freemem"])>0 {
 		debug.FreeOSMemory()
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
 
-	if r.Method=="POST" && len(r.Form["configjson"])>0 {
-		e := json.Unmarshal([]byte(r.Form["configjson"][0]), &common.CFG)
-		if e == nil {
-			common.Reset()
-		}
-		if len(r.Form["save"])>0 {
-			dat, _ := json.Marshal(&common.CFG)
-			if dat != nil {
-				ioutil.WriteFile(common.ConfigFile, dat, 0660)
-			}
-		}
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-
-	if r.Method=="POST" && len(r.Form["walletdata"])>0 && len(r.Form["walletfname"])>0 {
-		fn := r.Form["walletfname"][0]
-		if fn=="" {
-			fn = wallet.DefaultFileName
-		}
-		fn = common.GocoinHomeDir + "wallet" + string(os.PathSeparator) + fn
-		ioutil.WriteFile(fn, []byte(r.Form["walletdata"][0]), 0660)
-		wallet.LoadWallet(fn)
-		http.Redirect(w, r, "/wal", http.StatusFound)
-		return
-	}
-
-	if r.Method=="POST" && len(r.Form["shutdown"])>0 {
-		usif.Exit_now = true
-		w.Write([]byte("Your node should shut down soon"))
-		return
-	}
-
-	if checksid(r) && len(r.Form["mid"])>0 {
+	if len(r.Form["mid"])>0 {
 		v, e := strconv.ParseUint(r.Form["mid"][0], 10, 32)
 		if e == nil && v < uint64(len(common.MinerIds)) {
 			common.CFG.Beeps.MinerID = common.MinerIds[v][1]
@@ -109,4 +119,10 @@ func p_cfg(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "miners", http.StatusFound)
 		return
 	}
+
+	if len(r.Form["qwalsel"])>0 {
+		wallet.LoadWallet(common.GocoinHomeDir + "wallet" + string(os.PathSeparator) + r.Form["qwalsel"][0])
+		return
+	}
+
 }
