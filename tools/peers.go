71f71e5757860ac196c6da4fb5e427c544796bff
Piotr N <piotrnar@gmail.com>
2014-04-24 13:42:17 +0200

    Former-commit-id: cde1ec7f456d9a96710097bb5598d39fba2497e4

---
+++ tools/peers.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"sort"
+	"time"
+	"github.com/piotrnar/gocoin/qdb"
+	"github.com/piotrnar/gocoin/tools/utils"
+)
+
+type manyPeers []*utils.OnePeer
+
+func (mp manyPeers) Len() int {
+	return len(mp)
+}
+
+func (mp manyPeers) Less(i, j int) bool {
+	return mp[i].Time > mp[j].Time
+}
+
+func (mp manyPeers) Swap(i, j int) {
+	mp[i], mp[j] = mp[j], mp[i]
+}
+
+
+func main() {
+	var dir string
+
+	if len(os.Args)>1 {
+		dir = os.Args[1]
+	} else {
+		dir = utils.BitcoinHome() + "gocoin" + string(os.PathSeparator) + "btcnet" + string(os.PathSeparator) + "peers3"
+	}
+
+	db, er := qdb.NewDB(dir, true)
+
+	if er!=nil {
+		println(er.Error())
+		os.Exit(1)
+	}
+
+	println(db.Count(), "peers in databse", dir)
+	if db.Count()==0 {
+		return
+	}
+
+	tmp := make(manyPeers, db.Count())
+	cnt := 0
+	db.Browse(func(k qdb.KeyType, v []byte) uint32 {
+		np := utils.NewPeer(v)
+		if !utils.ValidIp4(np.Ip4[:]) {
+			return 0
+		}
+		if cnt < len(tmp) {
+			tmp[cnt] = np
+			cnt++
+		}
+		return 0
+	})
+
+	sort.Sort(tmp[:cnt])
+	for cnt=0; cnt<len(tmp)&&cnt<25; cnt++ {
+		ad := tmp[cnt]
+		fmt.Printf("%3d) %16s   %5d  - seen %5d min ago\n", cnt+1,
+			fmt.Sprintf("%d.%d.%d.%d", ad.Ip4[0], ad.Ip4[1], ad.Ip4[2], ad.Ip4[3]),
+			ad.Port, (time.Now().Unix() - int64(ad.Time))/60)
+	}
+}
