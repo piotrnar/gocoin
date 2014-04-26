b1a88611c936686541dd33707d362c9af48458a8
Piotr N <piotrnar@gmail.com>
2014-04-26 13:02:01 +0200

    Former-commit-id: 07e5f78392039a95c2bfe749ab24bcec458ea43d

---
+++ tools/base58.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"flag"
+	"strings"
+	"io/ioutil"
+	"encoding/hex"
+	"github.com/piotrnar/gocoin/btc"
+)
+
+var (
+	decode = flag.Bool("d", false, "run base58 decode (instead of encode)")
+	binary = flag.Bool("b", false, "binary (insted of hex) for decode output")
+	help = flag.Bool("h", false, "print this help")
+)
+
+func main() {
+	flag.Parse()
+	if *help {
+		flag.PrintDefaults()
+		return
+	}
+
+	msg, _ := ioutil.ReadAll(os.Stdin)
+	if len(msg)==0 {
+		return
+	}
+
+	if *decode {
+		res := btc.Decodeb58(strings.Trim(string(msg), " \t\n\r"))
+		if *binary {
+			os.Stdout.Write(res)
+		} else {
+			fmt.Println(hex.EncodeToString(res))
+		}
+	} else {
+		fmt.Println(btc.Encodeb58(msg))
+	}
+}
