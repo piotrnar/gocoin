a656d0fa5406d460e00ad0cab6975e3f7f84e8ac
Piotr N <piotrnar@gmail.com>
2014-05-03 17:51:40 +0200

    Former-commit-id: 173bf8107867bc98839bdd2c5c7817c0cdff18c4

---
+++ tools/utils/tx.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"io/ioutil"
+	"encoding/hex"
+)
+
+
+
+func is_hex_string(s []byte) (string) {
+	var res string
+	for i := range s {
+		c := byte(s[i])
+		if c<='9' && c>='0' || c<='f' && c>='a' || c<='F' && c>='A' {
+			res += string(c)
+		} else if c!=' ' && c!='\n' && c!='\r' && c!='\t' {
+			return ""
+		}
+	}
+	return res
+}
+
+// reads tx from the file or (if there is no such a file) decodes the text
+func GetRawData(fn string) (dat []byte) {
+	d, er := ioutil.ReadFile(fn)
+	if er != nil {
+		hexdump := is_hex_string(d)
+		if len(hexdump)>=2 || (len(hexdump)&1)==1 {
+			dat, _ = hex.DecodeString(hexdump)
+		} else {
+			dat = d
+		}
+	} else {
+		dat, _ = hex.DecodeString(fn)
+	}
+	return
+}
