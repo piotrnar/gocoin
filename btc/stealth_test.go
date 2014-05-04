df40aaf389ff76e8978f79e28f32efa0f5a2f5bb
Piotr N <piotrnar@gmail.com>
2014-05-04 12:34:47 +0200

    Former-commit-id: 61611d8bea65c484139b1b60248c5743eb9d562e

--- btc/stealth_test.go
+++ btc/stealth_test.go
@@ -56,3 +56,13 @@ func TestStealthAddr(t *testing.T) {
 			t.Error("Error expected")
 	}
 }
+
+
+func BenchmarkStealthDH(b *testing.B) {
+	xy, _ := hex.DecodeString("04d5b3853bbee336b551ff999b0b1d656e65a7649037ae0dcb02b3c4ff5f29e5beb389c8d94e6e8326b82367aee08d93f06bb4b344a40a2fe7024ffdcbab699f0f")
+	e, _ := hex.DecodeString("fa63521e333e4b9f6a98a142680d3aef4d8e7f79723ce0043691db55c36bd905")
+	b.ResetTimer()
+	for j := 0; j < b.N; j++ {
+		StealthDH(xy, e)
+	}
+}
