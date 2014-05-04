9af323d5f95c55c01d7852362160206e631ba35c
Piotr N <piotrnar@gmail.com>
2014-05-04 20:46:51 +0200

    Former-commit-id: 6be41957413ed1c3efb36106955cccd5bcc1afd4

--- btc/newec/newec_test.go
+++ btc/newec/newec_test.go
@@ -6,7 +6,8 @@ import (
 )
 
 var ta = [][3]string{
-	{ // [0]-pubScr, [1]-sigScript, [2]-unsignedTx
+	// [0]-pubScr, [1]-sigScript, [2]-unsignedTx
+	{
 		"040eaebcd1df2df853d66ce0e1b0fda07f67d1cabefde98514aad795b86a6ea66dbeb26b67d7a00e2447baeccc8a4cef7cd3cad67376ac1c5785aeebb4f6441c16",
 		"3045022100fe00e013c244062847045ae7eb73b03fca583e9aa5dbd030a8fd1c6dfcf11b1002207d0d04fed8fa1e93007468d5a9e134b0a7023b6d31db4e50942d43a250f4d07c01",
 		"3382219555ddbb5b00e0090f469e590ba1eae03c7f28ab937de330aa60294ed6",
