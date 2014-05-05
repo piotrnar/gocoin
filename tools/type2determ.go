07c9a4029d4453eac58925e60535624c6ae6b3fa
Piotr N <piotrnar@gmail.com>
2014-05-05 10:46:37 +0200

    Former-commit-id: 071dbc528a4ba6c1fe458de1d155f011eb52edbf

--- tools/type2determ.go
+++ tools/type2determ.go
@@ -1,6 +1,6 @@
 // This tool outpus Type-2 deterministic addresses, as described here:
 // https://bitcointalk.org/index.php?topic=19137.0
-// At input it takes "A_public_key" and "B_secret" - both values as hex encoded strings.
+// At input it takes "A_public_key" and "secret" - both values as hex encoded strings.
 // Optionally, you can add a third parameter - number of public keys you want to calculate.
 package main
 
@@ -8,7 +8,6 @@ import (
 	"os"
 	"fmt"
 	"strconv"
-	"math/big"
 	"encoding/hex"
 	"github.com/piotrnar/gocoin/btc"
 )
@@ -18,29 +17,29 @@ func main() {
 	var testnet bool
 
 	if len(os.Args) < 3 {
-		fmt.Println("Specify B_secret, A_public_key and optionaly number of addresses you want.")
+		fmt.Println("Specify secret, public_key and optionaly number of addresses you want.")
 		fmt.Println("Use a negative value for number of addresses, to work with Testnet addresses.")
 		return
 	}
-	A_public_key, er := hex.DecodeString(os.Args[2])
+	public_key, er := hex.DecodeString(os.Args[2])
 	if er != nil {
-		println("Error parsing A_public_key:", er.Error())
+		println("Error parsing public_key:", er.Error())
 		os.Exit(1)
 	}
 
-	pubk, er := btc.NewPublicKey(A_public_key)
-	if er != nil {
-		println("Invalid valid public key:", er.Error())
-		os.Exit(1)
+	if len(public_key)==33 && (public_key[0]==2 || public_key[0]==3) {
+		fmt.Println("Compressed")
+	} else if len(public_key)==65 && (public_key[0]==4) {
+		fmt.Println("Uncompressed")
+	} else {
+		println("Incorrect public key")
 	}
-	compressed := len(A_public_key)==33
 
-	B_secret, er := hex.DecodeString(os.Args[1])
+	secret, er := hex.DecodeString(os.Args[1])
 	if er != nil {
-		println("Error parsing B_secret:", er.Error())
+		println("Error parsing secret:", er.Error())
 		os.Exit(1)
 	}
-	sec := new(big.Int).SetBytes(B_secret)
 
 	n := int64(25)
 
@@ -61,15 +60,15 @@ func main() {
 	}
 
 	fmt.Println("# Type-2")
-	fmt.Println("#", hex.EncodeToString(pubk.Bytes(compressed)))
-	fmt.Println("#", hex.EncodeToString(sec.Bytes()))
+	fmt.Println("#", hex.EncodeToString(public_key))
+	fmt.Println("#", hex.EncodeToString(secret))
 
 	for i:=1; i<=int(n); i++ {
-		fmt.Println(btc.NewAddrFromPubkey(pubk.Bytes(compressed), btc.AddrVerPubkey(testnet)).String(), "TypB", i)
+		fmt.Println(btc.NewAddrFromPubkey(public_key, btc.AddrVerPubkey(testnet)).String(), "TypB", i)
 		if i >= int(n) {
 			break
 		}
 
-		pubk.X, pubk.Y = btc.DeriveNextPublic(pubk.X, pubk.Y, sec)
+		public_key = btc.DeriveNextPublic(public_key, secret)
 	}
 }
