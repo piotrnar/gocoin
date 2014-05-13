6929f8c20f6826921d6e638bc15993113564f363
Piotr N <piotrnar@gmail.com>
2014-05-13 21:04:51 +0200

    Former-commit-id: d3a0befef4b75b96d1181ba61d7cc83f995a6fe4

--- wallet/config.go
+++ wallet/config.go
@@ -27,91 +27,91 @@ func parse_config() {
 	d, e := ioutil.ReadFile(cfgfn)
 	if e != nil {
 		fmt.Println("wallet.cfg not found")
-		return
-	}
-	lines := strings.Split(string(d), "\n")
-	for i := range lines {
-		line := strings.Trim(lines[i], " \n\r\t")
-		if len(line)==0 || line[0]=='#' {
-			continue
-		}
+	} else {
+		lines := strings.Split(string(d), "\n")
+		for i := range lines {
+			line := strings.Trim(lines[i], " \n\r\t")
+			if len(line)==0 || line[0]=='#' {
+				continue
+			}
 
-		ll := strings.SplitN(line, "=", 2)
-		if len(ll)!=2 {
-			println(i, "wallet.cfg: syntax error in line", ll)
-			continue
-		}
+			ll := strings.SplitN(line, "=", 2)
+			if len(ll)!=2 {
+				println(i, "wallet.cfg: syntax error in line", ll)
+				continue
+			}
 
-		switch strings.ToLower(ll[0]) {
-			case "testnet":
-				v, e := strconv.ParseBool(ll[1])
-				if e == nil {
-					testnet = v
-				} else {
-					println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
-					os.Exit(1)
-				}
-
-			case "type":
-				v, e := strconv.ParseUint(ll[1], 10, 32)
-				if e == nil {
-					if v>=1 && v<=3 {
-						waltype = uint(v)
+			switch strings.ToLower(ll[0]) {
+				case "testnet":
+					v, e := strconv.ParseBool(ll[1])
+					if e == nil {
+						testnet = v
 					} else {
-						println(i, "wallet.cfg: incorrect wallet type", v)
+						println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
 						os.Exit(1)
 					}
-				} else {
-					println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
-					os.Exit(1)
-				}
 
-			case "type2sec":
-				type2sec = ll[1]
-
-			case "keycnt":
-				v, e := strconv.ParseUint(ll[1], 10, 32)
-				if e == nil {
-					if v>1 {
-						keycnt = uint(v)
+				case "type":
+					v, e := strconv.ParseUint(ll[1], 10, 32)
+					if e == nil {
+						if v>=1 && v<=3 {
+							waltype = uint(v)
+						} else {
+							println(i, "wallet.cfg: incorrect wallet type", v)
+							os.Exit(1)
+						}
 					} else {
-						println(i, "wallet.cfg: incorrect key count", v)
+						println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
 						os.Exit(1)
 					}
-				} else {
-					println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
-					os.Exit(1)
-				}
 
-			case "uncompressed":
-				v, e := strconv.ParseBool(ll[1])
-				if e == nil {
-					uncompressed = v
-				} else {
-					println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
-					os.Exit(1)
-				}
+				case "type2sec":
+					type2sec = ll[1]
 
-			// case "secrand": <-- deprecated
+				case "keycnt":
+					v, e := strconv.ParseUint(ll[1], 10, 32)
+					if e == nil {
+						if v>1 {
+							keycnt = uint(v)
+						} else {
+							println(i, "wallet.cfg: incorrect key count", v)
+							os.Exit(1)
+						}
+					} else {
+						println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
+						os.Exit(1)
+					}
 
-			case "fee":
-				fee = ll[1]
+				case "uncompressed":
+					v, e := strconv.ParseBool(ll[1])
+					if e == nil {
+						uncompressed = v
+					} else {
+						println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
+						os.Exit(1)
+					}
 
-			case "apply2bal":
-				v, e := strconv.ParseBool(ll[1])
-				if e == nil {
-					apply2bal = v
-				} else {
-					println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
-					os.Exit(1)
-				}
+				// case "secrand": <-- deprecated
 
-			case "secret":
-				PassSeedFilename = ll[1]
+				case "fee":
+					fee = ll[1]
 
-			case "others":
-				RawKeysFilename = ll[1]
+				case "apply2bal":
+					v, e := strconv.ParseBool(ll[1])
+					if e == nil {
+						apply2bal = v
+					} else {
+						println(i, "wallet.cfg: value error for", ll[0], ":", e.Error())
+						os.Exit(1)
+					}
 
+				case "secret":
+					PassSeedFilename = ll[1]
+
+				case "others":
+					RawKeysFilename = ll[1]
+
+			}
 		}
 	}
 
