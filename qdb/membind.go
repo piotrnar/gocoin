da4e3271aa416a193b295830f63e20c2b703e844
Piotr N <piotrnar@gmail.com>
2014-05-17 16:29:07 +0200

    Former-commit-id: 9d0dee4dea522106bdc0f1409dd5d0e502c35fb9

--- qdb/membind.go
+++ qdb/membind.go
@@ -8,10 +8,14 @@ package qdb
 #include <stdlib.h>
 #include <string.h>
 
-void *alloc_ptr(void *c, size_t l) {
+void *alloc_ptr(void *c, unsigned long l) {
 	void *ptr = malloc(l);
 	memcpy(ptr, c, l);
 	return ptr;
+}
+
+void *my_alloc(unsigned long l) {
+	return malloc(l);
 }
 
 */
@@ -41,7 +45,7 @@ func (v *oneIdx) Slice() (res []byte) {
 
 func newIdx(v []byte, f uint32) (r *oneIdx) {
 	r = new(oneIdx)
-	r.data = data_ptr_t(C.alloc_ptr(unsafe.Pointer(&v[0]), C.size_t(len(v))))
+	r.data = data_ptr_t(C.alloc_ptr(unsafe.Pointer(&v[0]), C.ulong(len(v))))
 	r.datlen = uint32(len(v))
 	atomic.AddInt64(&ExtraMemoryConsumed, int64(r.datlen))
 	r.flags = f
@@ -52,7 +56,7 @@ func (r *oneIdx) SetData(v []byte) {
 	if r.data!=nil {
 		panic("This should not happen")
 	}
-	r.data = data_ptr_t(C.alloc_ptr(unsafe.Pointer(&v[0]), C.size_t(len(v))))
+	r.data = data_ptr_t(C.alloc_ptr(unsafe.Pointer(&v[0]), C.ulong(len(v))))
 	atomic.AddInt64(&ExtraMemoryConsumed, int64(r.datlen))
 }
 
@@ -60,7 +64,7 @@ func (v *oneIdx) LoadData(f *os.File) {
 	if v.data!=nil {
 		C.free(unsafe.Pointer(v.data))
 	}
-	v.data = data_ptr_t(C.malloc(C.size_t(v.datlen)))
+	v.data = data_ptr_t(C.my_alloc(C.ulong(v.datlen)))
 	atomic.AddInt64(&ExtraMemoryConsumed, int64(v.datlen))
 	f.Seek(int64(v.datpos), os.SEEK_SET)
 	f.Read(*(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data:uintptr(v.data), Len:int(v.datlen), Cap:int(v.datlen)})))
