9507322006b0691d719117bcc72e4a24e6cf7831
Piotr N <piotrnar@gmail.com>
2014-05-25 14:45:47 +0200

    Former-commit-id: 2c555708e2ffe734c24211aed798d64c5f51c9ab Former-commit-id: c05a5846661983d4f762ecb86b6e3f997094ab4f

---
+++ lib/ltc/ltc.go
@@ -0,0 +1,37 @@
+package ltc
+
+import (
+	"bytes"
+	"github.com/piotrnar/gocoin/lib/btc"
+)
+
+const LTC_ADDR_VERSION = 48
+
+
+// LTC signing uses different seed string
+func HashFromMessage(msg []byte, out []byte) {
+	const MessageMagic = "Litecoin Signed Message:\n"
+	b := new(bytes.Buffer)
+	btc.WriteVlen(b, uint32(len(MessageMagic)))
+	b.Write([]byte(MessageMagic))
+	btc.WriteVlen(b, uint32(len(msg)))
+	b.Write(msg)
+	btc.ShaHash(b.Bytes(), out)
+}
+
+
+func AddrVerPubkey(testnet bool) byte {
+	if !testnet {
+		return LTC_ADDR_VERSION
+	}
+	return btc.AddrVerPubkey(testnet)
+}
+
+
+func NewAddrFromPkScript(scr []byte, testnet bool) (ad *btc.BtcAddr) {
+	ad = btc.NewAddrFromPkScript(scr, testnet)
+	if ad!=nil && ad.Version==btc.AddrVerPubkey(false) {
+		ad.Version = LTC_ADDR_VERSION
+	}
+	return
+}
