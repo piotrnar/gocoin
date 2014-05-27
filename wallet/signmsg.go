1138c4dc12f87e44e7b16139f6a59f670fa1d9be
Piotr N <piotrnar@gmail.com>
2014-05-27 16:43:06 +0200

    Former-commit-id: be59ef354df14270a3fa54c711681bd4ccf5d7aa Former-commit-id: f146e285a2aa3e8c58976710c03e9a74f9f6daf7

--- wallet/signmsg.go
+++ wallet/signmsg.go
@@ -14,6 +14,7 @@ import (
 // this function signs either a message or a raw transaction hash
 func sign_message() {
 	var hash []byte
+	var signkey *btc.PrivateAddr
 
 	if *signhash!="" {
 		var er error
@@ -34,35 +35,30 @@ func sign_message() {
 		return
 	}
 
-	var privkey []byte
-	var compr bool
 
 	for i := range keys {
 		if keys[i].BtcAddr.Hash160==ad2s.Hash160 {
-			privkey = keys[i].Key
-			compr = keys[i].BtcAddr.IsCompressed()
-
-			// Sign raw hash?
-			if hash!=nil {
-				txsig := new(btc.Signature)
-				txsig.HashType = 0x01
-				r, s, e := btc.EcdsaSign(privkey, hash)
-				if e != nil {
-					println(e.Error())
-					return
-				}
-				txsig.R.Set(r)
-				txsig.S.Set(s)
-				fmt.Println("PublicKey:", hex.EncodeToString(keys[i].BtcAddr.Pubkey))
-				fmt.Println(hex.EncodeToString(txsig.Bytes()))
-				return
-			}
-
+			signkey = keys[i]
 			break
 		}
 	}
-	if privkey==nil {
+	if signkey==nil {
 		println("You do not have a private key for", ad2s.String())
+		return
+	}
+
+	if hash!=nil { // special case for signing a hash (not message)
+		txsig := new(btc.Signature)
+		txsig.HashType = 0x01
+		r, s, e := btc.EcdsaSign(signkey.Key, hash)
+		if e != nil {
+			println(e.Error())
+			return
+		}
+		txsig.R.Set(r)
+		txsig.S.Set(s)
+		fmt.Println("PublicKey:", hex.EncodeToString(signkey.BtcAddr.Pubkey))
+		fmt.Println(hex.EncodeToString(txsig.Bytes()))
 		return
 	}
 
@@ -84,11 +80,11 @@ func sign_message() {
 	btcsig := new(btc.Signature)
 	var sb [65]byte
 	sb[0] = 27
-	if compr {
+	if signkey.IsCompressed() {
 		sb[0] += 4
 	}
 
-	r, s, e := btc.EcdsaSign(privkey, hash)
+	r, s, e := btc.EcdsaSign(signkey.Key, hash)
 	if e != nil {
 		println(e.Error())
 		return
@@ -102,14 +98,14 @@ func sign_message() {
 	copy(sb[1+64-len(sd):], sd)
 
 	rpk := btcsig.RecoverPublicKey(hash[:], 0)
-	sa := btc.NewAddrFromPubkey(rpk.Bytes(compr), ad2s.Version)
+	sa := btc.NewAddrFromPubkey(rpk.Bytes(signkey.IsCompressed()), ad2s.Version)
 	if sa.Hash160==ad2s.Hash160 {
 		fmt.Println(base64.StdEncoding.EncodeToString(sb[:]))
 		return
 	}
 
 	rpk = btcsig.RecoverPublicKey(hash[:], 1)
-	sa = btc.NewAddrFromPubkey(rpk.Bytes(compr), ad2s.Version)
+	sa = btc.NewAddrFromPubkey(rpk.Bytes(signkey.IsCompressed()), ad2s.Version)
 	if sa.Hash160==ad2s.Hash160 {
 		sb[0]++
 		fmt.Println(base64.StdEncoding.EncodeToString(sb[:]))
