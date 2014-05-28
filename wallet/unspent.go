9ffb3575ee467a254a0862d42b99f6f2de9bf848
Piotr N <piotrnar@gmail.com>
2014-05-28 14:11:08 +0200

    Former-commit-id: 24e7db6f2259484fbf667ddc833ccd3ccddd039c Former-commit-id: 6f441bfb908ba398c533139054c2fffe683364bf

--- wallet/unspent.go
+++ wallet/unspent.go
@@ -6,22 +6,81 @@ import (
 	"bufio"
 	"strconv"
 	"strings"
+	"io/ioutil"
 	"encoding/hex"
 	"github.com/piotrnar/gocoin/lib/btc"
 )
 
+
+type unspRec struct {
+	btc.TxPrevOut
+	label string
+	key *btc.PrivateAddr
+	stealth bool
+	spent bool
+}
+
 var (
 	// set in load_balance():
-	unspentOuts []*btc.TxPrevOut
-	unspentOutsLabel []string
+	unspentOuts []*unspRec
 	loadedTxs map[[32]byte] *btc.Tx = make(map[[32]byte] *btc.Tx)
 	totBtc uint64
 )
 
+func (u *unspRec) String() string {
+	return fmt.Sprint(u.TxPrevOut.String(), " ", u.label)
+}
+
+func NewUnspRec(l []byte) (uns *unspRec) {
+	if l[64]!='-' {
+		return nil
+	}
+
+	txid := btc.NewUint256FromString(string(l[:64]))
+	if txid==nil {
+		return nil
+	}
+
+	rst := strings.SplitN(string(l[65:]), " ", 2)
+	vout, e := strconv.ParseUint(rst[0], 10, 32)
+	if e != nil {
+		return nil
+	}
+
+	uns = new(unspRec)
+	uns.TxPrevOut.Hash = txid.Hash
+	uns.TxPrevOut.Vout = uint32(vout)
+	if len(rst)>1 {
+		uns.label = rst[1]
+	}
+
+	str := string(l)
+	if sti:=strings.Index(str, "_StealthC:"); sti!=-1 {
+		c, e := hex.DecodeString(str[sti+10:sti+10+64])
+		if e != nil {
+			fmt.Println("ERROR at stealth", txid.String(), vout, e.Error())
+		} else {
+			// add a new key to the wallet
+			sec := btc.DeriveNextPrivate(keys[first_determ_idx].Key, c)
+			rec := btc.NewPrivateAddr(sec, ver_secret(), true) // stealth keys are always compressed
+			rec.BtcAddr.Extra.Label = uns.label
+			keys = append(keys, rec)
+			uns.stealth = true
+			uns.key = rec
+		}
+	}
+
+	if _, ok := loadedTxs[txid.Hash]; !ok {
+		loadedTxs[txid.Hash] = tx_from_balance(txid, true)
+	}
+
+	return
+}
+
 
 // load the content of the "balance/" folder
 func load_balance(showbalance bool) {
-	var unknownInputs, multisigInputs int
+	var unknownInputs int
 	f, e := os.Open("balance/unspent.txt")
 	if e != nil {
 		println(e.Error())
@@ -33,96 +92,37 @@ func load_balance(showbalance bool) {
 		if len(l)==0 && e!=nil {
 			break
 		}
-		if l[64]=='-' {
-			txid := btc.NewUint256FromString(string(l[:64]))
-			rst := strings.SplitN(string(l[65:]), " ", 2)
-			vout, _ := strconv.ParseUint(rst[0], 10, 32)
-			uns := new(btc.TxPrevOut)
-			copy(uns.Hash[:], txid.Hash[:])
-			uns.Vout = uint32(vout)
-			lab := ""
-			if len(rst)>1 {
-				lab = rst[1]
-			}
 
-			str := string(l)
-			if sti:=strings.Index(str, "_StealthC:"); sti!=-1 {
-				c, e := hex.DecodeString(str[sti+10:sti+10+64])
-				if e != nil {
-					fmt.Println("ERROR at stealth", txid.String(), vout, e.Error())
-				} else {
-					// add a new key to the wallet
-					sec := btc.DeriveNextPrivate(keys[first_determ_idx].Key, c)
-					is_stealth[len(keys)] = true
-					rec := btc.NewPrivateAddr(sec, ver_secret(), true) // stealth keys are always compressed
-					rec.BtcAddr.Extra.Label = lab
-					keys = append(keys, rec)
-				}
-			}
-
-			if _, ok := loadedTxs[txid.Hash]; !ok {
-				tx := tx_from_balance(txid, true)
-				loadedTxs[txid.Hash] = tx
-			}
-
-			// Sum up all the balance and check if we have private key for this input
+		if uns:=NewUnspRec(l); uns!=nil {
 			uo := UO(uns)
-
-			add_it := true
-
-			if !btc.IsP2SH(uo.Pk_script) {
-				fnd := false
-				for j := range keys {
-					if keys[j].BtcAddr.Owns(uo.Pk_script) {
-						fnd = true
-						break
-					}
-				}
-
-				if !fnd {
-					if *onlvalid {
-						add_it = false
-					}
-					if showbalance {
-						unknownInputs++
-						if *verbose {
-							ss := uns.String()
-							ss = ss[:8]+"..."+ss[len(ss)-12:]
-							fmt.Println(ss, "does not belong to your wallet (cannot sign it)")
-						}
-					}
-				}
+			if uns.key==nil {
+				uns.key = pkscr_to_key(uo.Pk_script)
+			}
+			// Sum up all the balance and check if we have private key for this input
+			if uns.key!=nil {
+				totBtc += uo.Value
 			} else {
-				if *onlvalid {
-					add_it = false
-				}
+				unknownInputs++
 				if *verbose {
-					ss := uns.String()
-					ss = ss[:8]+"..."+ss[len(ss)-12:]
-					fmt.Println(ss, "belongs to a multisig address")
+					fmt.Println("WARNING: Don't know how to sign", uns.TxPrevOut.String())
 				}
-				multisigInputs++
 			}
-
-			if add_it {
-				unspentOuts = append(unspentOuts, uns)
-				unspentOutsLabel = append(unspentOutsLabel, lab)
-				totBtc += UO(uns).Value
-			}
+			unspentOuts = append(unspentOuts, uns)
+		} else {
+			println("ERROR in unspent.txt: ", string(l))
 		}
 	}
 	f.Close()
-	fmt.Printf("You have %.8f BTC in %d unspent outputs. %d inputs are multisig type\n",
-		float64(totBtc)/1e8, len(unspentOuts), multisigInputs)
-	if showbalance {
-		if unknownInputs > 0 {
-			fmt.Printf("WARNING: Some inputs (%d) cannot be spent with this password (-v to print them)\n", unknownInputs);
-		}
+	fmt.Printf("You have %.8f BTC in %d unspent outputs\n",
+		float64(totBtc)/1e8, len(unspentOuts)-unknownInputs)
+	if showbalance && unknownInputs > 0 {
+		fmt.Println("WARNING:", unknownInputs, "unspendable input(s). Add -v switch to print them.")
 	}
 }
 
 // Get TxOut record, by the given TxPrevOut
-func UO(uns *btc.TxPrevOut) *btc.TxOut {
+func UO(rec *unspRec) *btc.TxOut {
+	uns := rec.TxPrevOut
 	tx, _ := loadedTxs[uns.Hash]
 	if tx==nil {
 		println("Unknown content of input", uns.String())
@@ -144,37 +144,31 @@ func getUO(pto *btc.TxPrevOut) *btc.TxOut {
 
 // apply the chnages to the balance folder
 func apply_to_balance(tx *btc.Tx) {
-	fmt.Println("Applying the transaction to the balance/ folder...")
 	f, _ := os.Create("balance/unspent.txt")
 	if f != nil {
-		for j:=len(tx.TxIn); j<len(unspentOuts); j++ {
-			fmt.Fprintln(f, unspentOuts[j], unspentOutsLabel[j])
-		}
-		if *verbose {
-			fmt.Println(len(tx.TxIn), "spent output(s) removed from 'balance/unspent.txt'")
+		// append new outputs at the end of unspentOuts
+		ioutil.WriteFile("balance/"+tx.Hash.String()+".tx", tx.Serialize(), 0600)
+
+		fmt.Println("Adding", len(tx.TxOut), "new output(s) to the balance/ folder...")
+		for out := range tx.TxOut {
+			if k:=pkscr_to_key(tx.TxOut[out].Pk_script); k!=nil {
+				uns := new(unspRec)
+				uns.key = k
+				uns.TxPrevOut.Hash = tx.Hash.Hash
+				uns.TxPrevOut.Vout = uint32(out)
+				uns.label = fmt.Sprint("# ", btc.UintToBtc(tx.TxOut[out].Value), " BTC @ ", k.BtcAddr.String())
+				//stealth bool TODO: maybe we can fix it...
+				unspentOuts = append(unspentOuts, uns)
+			}
 		}
 
-		var addback int
-		for out := range tx.TxOut {
-			for j := range keys {
-				if keys[j].BtcAddr.Owns(tx.TxOut[out].Pk_script) {
-					fmt.Fprintf(f, "%s-%03d # %.8f BTC @ %s\n", tx.Hash.String(), out,
-						float64(tx.TxOut[out].Value)/1e8, keys[j].BtcAddr.String())
-					addback++
-				}
+		for j:= range unspentOuts {
+			if !unspentOuts[j].spent {
+				fmt.Fprintln(f, unspentOuts[j].String())
 			}
 		}
 		f.Close()
-
-		if addback > 0 {
-			f, _ = os.Create("balance/"+tx.Hash.String()+".tx")
-			if f != nil {
-				f.Write(tx.Serialize())
-				f.Close()
-			}
-			if *verbose {
-				fmt.Println(addback, "new output(s) appended to 'balance/unspent.txt'")
-			}
-		}
+	} else {
+		println("ERROR: Cannot create balance/unspent.txt")
 	}
 }
