2d72bcfc91e00e099099f29ce3851bc7b4396cf0
Piotr N <piotrnar@gmail.com>
2014-06-06 11:02:18 +0200

    Former-commit-id: 06088ca1e452bf7f2b82a158266b51a6b4034886 Former-commit-id: 2126a49d24a3ca0030731abb3c4a6707e60768d0

--- lib/chain/const.go
+++ lib/chain/const.go
@@ -5,4 +5,5 @@ const(
 	MovingCheckopintDepth = 2016  // Do not accept forks that wold go deeper in a past
 	GenesisBlockTime = 1231006505
 	BIP16SwitchTime = 1333238400 // BIP16 didn't become active until Apr 1 2012
+	ForceBlockVer2From = 200000 // we just use fixed block number and do not enfoce it for testnet
 )
