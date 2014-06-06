9eaf9968213b3a0d8acb1657862593a934843dba
Piotr N <piotrnar@gmail.com>
2014-06-06 12:35:47 +0200

    Former-commit-id: 34a7a2c73548128abc68ff774ec3fa1b5c82eb33 Former-commit-id: eb2e3efddbf031edffd4d78754c1e4554df0f8a4

---
+++ client/speedups/sipadll.go
@@ -0,0 +1,35 @@
+package main
+
+/*
+  This is a EC_Verify speedup that works inly with Windows
+
+  Use secp256k1.dll from gocoin/tools/sipa_dll
+  or build one yourself.
+
+*/
+
+import (
+	"fmt"
+	"unsafe"
+	"syscall"
+	"github.com/piotrnar/gocoin/lib/btc"
+)
+
+var (
+	advapi32 = syscall.NewLazyDLL("secp256k1.dll")
+	DLL_EC_Verify = advapi32.NewProc("EC_Verify")
+)
+
+
+func EC_Verify(pkey, sign, hash []byte) bool {
+	r1, _, _ := syscall.Syscall6(DLL_EC_Verify.Addr(), 6,
+		uintptr(unsafe.Pointer(&hash[0])), uintptr(32),
+		uintptr(unsafe.Pointer(&sign[0])), uintptr(len(sign)),
+		uintptr(unsafe.Pointer(&pkey[0])), uintptr(len(pkey)))
+	return r1==1
+}
+
+func init() {
+	fmt.Println("Using secp256k1.dll by sipa for EC_Verify")
+	btc.EC_Verify = EC_Verify
+}
