b1998cf4b521e7ddd3a28ebd7a0d5c50092f67fa
Piotr N <piotrnar@gmail.com>
2014-06-07 10:36:33 +0200

    Former-commit-id: 7740389a320d71497865fbe4c97cb799b2797b65 Former-commit-id: a4bcb0e672e5254f673b524d45ff016db61e3831

--- lib/others/peersdb/peerdb.go
+++ lib/others/peersdb/peerdb.go
@@ -13,7 +13,6 @@ import (
 	"github.com/piotrnar/gocoin/lib/qdb"
 	"github.com/piotrnar/gocoin/lib/others/sys"
 	"github.com/piotrnar/gocoin/lib/others/utils"
-	"github.com/piotrnar/gocoin/client/common"
 )
 
 const (
@@ -24,12 +23,16 @@ var (
 	PeerDB *qdb.DB
 	proxyPeer *PeerAddr // when this is not nil we should only connect to this single node
 	peerdb_mutex sync.Mutex
+
+	Testnet bool
+	ConnectOnly string
+	Services uint64 = 1
+	DefaultTcpPort uint16 = 8333
 )
 
 type PeerAddr struct {
 	*utils.OnePeer
 }
-
 
 func NewEmptyPeer() (p *PeerAddr) {
 	p = new(PeerAddr)
@@ -44,22 +47,22 @@ func NewPeer(v []byte) (p *PeerAddr) {
 }
 
 
-func NewIncomingPeer(ipstr string) (p *PeerAddr, e error) {
+func NewPeerFromString(ipstr string) (p *PeerAddr, e error) {
 	x := strings.Index(ipstr, ":")
 	if x != -1 {
 		ipstr = ipstr[:x] // remove port number
 	}
 	ip := net.ParseIP(ipstr)
 	if ip != nil && len(ip)==16 {
-		if common.IsIPBlocked(ip[12:16]) {
+		if sys.IsIPBlocked(ip[12:16]) {
 			e = errors.New(ipstr+" is blocked")
 			return
 		}
 		p = NewEmptyPeer()
 		copy(p.Ip4[:], ip[12:16])
-		p.Services = common.Services
+		p.Services = Services
 		copy(p.Ip6[:], ip[:12])
-		p.Port = common.DefaultTcpPort
+		p.Port = DefaultTcpPort
 		if dbp := PeerDB.Get(qdb.KeyType(p.UniqID())); dbp!=nil && NewPeer(dbp).Banned!=0 {
 			e = errors.New(p.Ip() + " is banned")
 			p = nil
@@ -88,15 +91,11 @@ func ExpirePeers() {
 		return 0
 	})
 	if delcnt > 0 {
-		common.CountSafeAdd("PeersExpired", uint64(delcnt))
 		for delcnt > 0 {
 			delcnt--
 			PeerDB.Del(todel[delcnt])
 		}
-		common.CountSafe("PeerDefragsDone")
 		PeerDB.Defrag()
-	} else {
-		common.CountSafe("PeerDefragsNone")
 	}
 	peerdb_mutex.Unlock()
 }
@@ -174,7 +173,7 @@ func GetBestPeers(limit uint, isConnected func(*PeerAddr)bool) (res manyPeers) {
 	tmp := make(manyPeers, 0)
 	PeerDB.Browse(func(k qdb.KeyType, v []byte) uint32 {
 		ad := NewPeer(v)
-		if ad.Banned==0 && sys.ValidIp4(ad.Ip4[:]) && !common.IsIPBlocked(ad.Ip4[:]) {
+		if ad.Banned==0 && sys.ValidIp4(ad.Ip4[:]) && !sys.IsIPBlocked(ad.Ip4[:]) {
 			if isConnected==nil || !isConnected(ad) {
 				tmp = append(tmp, ad)
 			}
@@ -222,25 +221,25 @@ func initSeeds(seeds []string, port uint16) {
 func InitPeers(dir string) {
 	PeerDB, _ = qdb.NewDB(dir+"peers3", true)
 
-	if common.CFG.ConnectOnly != "" {
-		x := strings.Index(common.CFG.ConnectOnly, ":")
+	if ConnectOnly != "" {
+		x := strings.Index(ConnectOnly, ":")
 		if x == -1 {
-			common.CFG.ConnectOnly = fmt.Sprint(common.CFG.ConnectOnly, ":", common.DefaultTcpPort)
+			ConnectOnly = fmt.Sprint(ConnectOnly, ":", DefaultTcpPort)
 		}
-		oa, e := net.ResolveTCPAddr("tcp4", common.CFG.ConnectOnly)
+		oa, e := net.ResolveTCPAddr("tcp4", ConnectOnly)
 		if e != nil {
 			println(e.Error())
 			os.Exit(1)
 		}
 		proxyPeer = NewEmptyPeer()
-		proxyPeer.Services = common.Services
+		proxyPeer.Services = Services
 		copy(proxyPeer.Ip4[:], oa.IP[0:4])
 		proxyPeer.Port = uint16(oa.Port)
 		fmt.Printf("Connect to bitcoin network via %d.%d.%d.%d:%d\n",
 			oa.IP[0], oa.IP[1], oa.IP[2], oa.IP[3], oa.Port)
 	} else {
 		go func() {
-			if !common.CFG.Testnet {
+			if !Testnet {
 				initSeeds([]string{
 					"seed.bitcoin.sipa.be",
 					"dnsseed.bluematt.me",
