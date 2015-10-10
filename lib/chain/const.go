0e812642eb6cc28da302e3d735f7ab67f9213f3f
Piotr Narewski <piotrnar@gmail.com>
2015-10-10 17:26:15 +0200

    Former-commit-id: 21d416c35f736ea8eea5790d97c7cc4d64b3166b Former-commit-id: 4845f0ba02c1e523fc3e6e1adf4fb1c852f79814

--- lib/chain/const.go
+++ lib/chain/const.go
@@ -1,7 +1,7 @@
 package chain
 
 const(
-	BlockMapInitLen = 400e3
+	BlockMapInitLen = 500e3
 	MovingCheckopintDepth = 2016  // Do not accept forks that wold go deeper in a past
 	GenesisBlockTime = 1231006505
 	BIP16SwitchTime = 1333238400 // BIP16 didn't become active until Apr 1 2012
