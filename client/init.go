9190a1d93d3bfeecc72142668e5208f9a13d6129
Piotr Narewski <piotrnar@gmail.com>
2016-03-16 10:35:03 +0100

    Former-commit-id: 1a8797656dff4544bd7f37dc5169f6267c0d6e87 Former-commit-id: c955acdefdbacecaf37f96c4e9a7d233516f1f13

--- client/init.go
+++ client/init.go
@@ -30,13 +30,13 @@ func host_init() {
 		common.GocoinHomeDir += common.DataSubdir() + string(os.PathSeparator)
 		BtcRootDir += "testnet3"+string(os.PathSeparator)
 		network.AlertPubKey, _ = hex.DecodeString("04302390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a")
-		common.MaxPeersNeeded = 100
+		common.MaxPeersNeeded = 2000
 	} else {
 		common.GenesisBlock = btc.NewUint256FromString("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
 		common.Magic = [4]byte{0xF9,0xBE,0xB4,0xD9}
 		common.GocoinHomeDir += common.DataSubdir() + string(os.PathSeparator)
 		network.AlertPubKey, _ = hex.DecodeString("04fc9702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284")
-		common.MaxPeersNeeded = 1000
+		common.MaxPeersNeeded = 5000
 	}
 
 	// Lock the folder
