fdb57bfc06569eb4a2fb859c6d882c1d07166ce2
Piotr Narewski <piotrnar@gmail.com>
2016-03-20 17:30:19 +0100

    Former-commit-id: 3278a095a106bd554dcd42f16b86da70a7898058 Former-commit-id: 669869e015686fb5ae03ff8cbfe16852b1cbbf04

--- client/rpcapi/mining.go
+++ client/rpcapi/mining.go
@@ -15,7 +15,7 @@ const MAX_TXS_LEN = 999e3 // 999KB, with 1KB margin to not exceed 1MB with conib
 type OneTransaction struct {
 	Data string `json:"data"`
 	Hash string `json:"hash"`
-	Depends []int `json:"depends"`
+	Depends []uint `json:"depends"`
 	Fee uint64 `json:"fee"`
 	Sigops uint `json:"sigops"`
 }
@@ -85,45 +85,103 @@ func GetNextBlockTemplate(r *GetBlockTemplateResp) {
 
 
 /* memory pool transaction sorting stuff */
-type sortedTxList []*network.OneTxToSend
+type one_mining_tx struct {
+	*network.OneTxToSend
+	depends []uint
+	startat int
+}
+
+type sortedTxList []*one_mining_tx
 func (tl sortedTxList) Len() int {return len(tl)}
 func (tl sortedTxList) Swap(i, j int)      { tl[i], tl[j] = tl[j], tl[i] }
 func (tl sortedTxList) Less(i, j int) bool { return tl[j].Fee < tl[i].Fee }
 
-func GetTransactions() (res []OneTransaction, totfees uint64) {
-	network.TxMutex.Lock()
-	defer network.TxMutex.Unlock()
 
-	sorted := make(sortedTxList, len(network.TransactionsToSend))
-	var cnt int
+var txs_so_far map[[32]byte] uint
+var totlen int
+
+func get_next_tranche_of_txs() (res sortedTxList) {
+	var unsp *btc.TxOut
+	var all_inputs_found bool
 	for _, v := range network.TransactionsToSend {
-		if v.MemInputs {
-			continue // skip meminput txs for now
+		tx := v.Tx
+
+		if _, ok := txs_so_far[tx.Hash.Hash]; ok {
+			continue
 		}
-		sorted[cnt] = v
-		cnt++
-	}
-	sorted = sorted[:cnt]
-	sort.Sort(sorted)
-
-	res = make([]OneTransaction, len(sorted))
-	totlen := 1000
-	for cnt=0; cnt<len(sorted); cnt++ {
-		v := sorted[cnt]
-
+		
 		if totlen+len(v.Data) > btc.MAX_BLOCK_SIZE {
 			println("Too many txs - limit to 999000 bytes")
-			res = res[:cnt]
 			return
 		}
 		totlen += len(v.Data)
 
+		all_inputs_found = true
+		var depends []uint
+		for i := range tx.TxIn {
+			unsp, _ = common.BlockChain.Unspent.UnspentGet(&tx.TxIn[i].Input)
+			if unsp==nil { 
+				// not found in the confirmed blocks
+				// check if txid is in txs_so_far
+				if idx, ok := txs_so_far[tx.TxIn[i].Input.Hash]; !ok {
+					// also not in txs_so_far
+					all_inputs_found = false
+					break
+				} else {
+					depends = append(depends, idx)
+				}
+			}
+		}
+
+		if all_inputs_found {
+			res = append(res, &one_mining_tx{OneTxToSend:v, depends:depends, startat:1+len(txs_so_far)})
+		}
+	}
+	return
+}
+
+func GetTransactions() (res []OneTransaction, totfees uint64) {
+	
+	network.TxMutex.Lock()
+	defer network.TxMutex.Unlock()
+
+	var cnt int
+	var sorted sortedTxList
+	txs_so_far = make(map[[32]byte]uint)
+	totlen = 0
+	println("\ngetting txs from the pool of", len(network.TransactionsToSend), "...")
+	for {
+		new_piece := get_next_tranche_of_txs()
+		if new_piece.Len()==0 {
+			break
+		}
+		//println("adding another", len(new_piece))
+		sort.Sort(new_piece)
+
+		for i:=0; i<len(new_piece); i++ {
+			txs_so_far[new_piece[i].Tx.Hash.Hash] = uint(1+len(sorted)+i)
+		}
+
+		sorted = append(sorted, new_piece...)
+	}
+	if len(txs_so_far)!=len(network.TransactionsToSend) {
+		println("txs_so_far len", len(txs_so_far))
+	}
+	txs_so_far = nil // leave it for the garbage collector
+
+	res = make([]OneTransaction, len(sorted))
+	for cnt=0; cnt<len(sorted); cnt++ {
+		v := sorted[cnt]
 		res[cnt].Data = hex.EncodeToString(v.Data)
 		res[cnt].Hash = v.Tx.Hash.String()
-		//res[cnt].Depends
 		res[cnt].Fee = v.Fee
 		res[cnt].Sigops = v.Tx.Sigops
+		res[cnt].Depends = v.depends
 		totfees += v.Fee
+		println("", cnt+1, v.Tx.Hash.String(), "  turn:", v.startat,
+			"  spb:", int(v.Fee)/len(v.Data), "  depend:", fmt.Sprint(v.depends))
 	}
+
+	println("returning transacitons:", totlen, len(res))
 	return
 }
