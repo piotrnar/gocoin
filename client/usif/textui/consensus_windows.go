a6b1a7ad12f7c15d3f8923cd2f8ee7185ff0a0ce
Piotr Narewski <piotrnar@gmail.com>
2016-04-02 08:34:25 +0200

    Former-commit-id: efeb156271f09827f4862672dbfa4f347cc8f062 Former-commit-id: a5ae3505ec99d5115d499f67a6df522e64e3cb18

--- client/usif/textui/consensus_windows.go
+++ client/usif/textui/consensus_windows.go
@@ -35,6 +35,8 @@ var (
 
 
 func check_consensus(pkScr []byte, i int, tx *btc.Tx, ver_flags uint32, result bool) {
+	tmp := make([]byte, len(pkScr))
+	copy(tmp, pkScr)
 	go func(pkScr []byte, txTo []byte, i int, ver_flags uint32, result bool) {
 		var pkscr_ptr uintptr
 		if pkScr != nil {
@@ -63,7 +65,7 @@ func check_consensus(pkScr []byte, i int, tx *btc.Tx, ver_flags uint32, result b
 			println("ver_flags", ver_flags)
 			mut.Unlock()
 		}
-	}(pkScr, tx.Serialize(), i, ver_flags, result)
+	}(tmp, tx.Serialize(), i, ver_flags, result)
 }
 
 func consensus_stats(s string) {
