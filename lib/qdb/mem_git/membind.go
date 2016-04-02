cd86aa5d095e967390f2491477a86df4effe3601
Piotr Narewski <piotrnar@gmail.com>
2016-04-02 08:55:42 +0200

    Former-commit-id: af873f68c6792a3a555da977d8888dbec1d28c9a Former-commit-id: 7bcbbd09d13dc1b6c06c7f0ba5259e80ace59d46

---
+++ lib/qdb/mem_git/membind.go
@@ -0,0 +1,40 @@
+/*
+You can replace this file with either membind_gcc.go or membind_windows.go
+to possibly improve memory usage of your node.
+*/
+
+package qdb
+
+import "os"
+
+type data_ptr_t []byte
+
+func (v *oneIdx) FreeData() {
+	v.data = nil
+}
+
+func (v *oneIdx) Slice() (res []byte) {
+	return v.data
+}
+
+func newIdx(v []byte, f uint32) (r *oneIdx) {
+	r = new(oneIdx)
+	r.data = v
+	r.datlen = uint32(len(v))
+	r.flags = f
+	return
+}
+
+func (r *oneIdx) SetData(v []byte) {
+	r.data = v
+}
+
+func (v *oneIdx) LoadData(f *os.File) {
+	v.data = make([]byte, int(v.datlen))
+	f.Seek(int64(v.datpos), os.SEEK_SET)
+	f.Read(v.data)
+}
+
+func init() {
+	println("Using Go native heap for qdb records. Try replacing qdb/membind.go with one from mem_gcc/ or mem_win/")
+}
