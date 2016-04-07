daae38cc682e8d27a2d6653619637b3b96dc08ff
Piotr Narewski <piotrnar@gmail.com>
2016-04-07 12:23:26 +0200

    Former-commit-id: 800c9c8c567c52987be659ebeae075c0b53c6ce7 Former-commit-id: 3ad9f0094f1b6f39b347cbb6cf44045678210a20

--- lib/qdb/membind.go
+++ lib/qdb/membind.go
@@ -5,36 +5,76 @@ to possibly improve memory usage of your node.
 
 package qdb
 
-import "os"
+import (
+	"os"
+	"unsafe"
+	"reflect"
+	"sync/atomic"
+)
 
-type data_ptr_t []byte
+var (
+	membind_use_wrapper bool
+	_HeapAlloc func(le uint32) data_ptr_t
+	_HeapFree func(ptr data_ptr_t)
+	_AllocPtr func(v []byte) data_ptr_t
+)
+
+
+type data_ptr_t unsafe.Pointer
 
 func (v *oneIdx) FreeData() {
+	if membind_use_wrapper {
+		_HeapFree(v.data)
+		atomic.AddInt64(&ExtraMemoryConsumed, -int64(v.datlen))
+		atomic.AddInt64(&ExtraMemoryAllocCnt, -1)
+	}
 	v.data = nil
 }
 
 func (v *oneIdx) Slice() (res []byte) {
-	return v.data
+	if membind_use_wrapper {
+		res = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data:uintptr(v.data), Len:int(v.datlen), Cap:int(v.datlen)}))
+	} else {
+		res = *(*[]byte)(v.data)
+	}
+	return
 }
 
 func newIdx(v []byte, f uint32) (r *oneIdx) {
 	r = new(oneIdx)
-	r.data = v
 	r.datlen = uint32(len(v))
+	if membind_use_wrapper {
+		r.data = _AllocPtr(v)
+		atomic.AddInt64(&ExtraMemoryConsumed, int64(r.datlen))
+		atomic.AddInt64(&ExtraMemoryAllocCnt, 1)
+	} else {
+		r.data = data_ptr_t(&v)
+	}
 	r.flags = f
 	return
 }
 
 func (r *oneIdx) SetData(v []byte) {
-	r.data = v
+	if membind_use_wrapper {
+		r.data = _AllocPtr(v)
+		atomic.AddInt64(&ExtraMemoryConsumed, int64(r.datlen))
+		atomic.AddInt64(&ExtraMemoryAllocCnt, 1)
+	} else {
+		r.data = data_ptr_t(&v)
+	}
 }
 
 func (v *oneIdx) LoadData(f *os.File) {
-	v.data = make([]byte, int(v.datlen))
-	f.Seek(int64(v.datpos), os.SEEK_SET)
-	f.Read(v.data)
-}
-
-func init() {
-	println("Using Go native heap for qdb records. Try replacing qdb/membind.go with one from mem_gcc/ or mem_win/")
+	if membind_use_wrapper {
+		v.data = _HeapAlloc(v.datlen)
+		atomic.AddInt64(&ExtraMemoryConsumed, int64(v.datlen))
+		atomic.AddInt64(&ExtraMemoryAllocCnt, 1)
+		f.Seek(int64(v.datpos), os.SEEK_SET)
+		f.Read(*(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data:uintptr(v.data), Len:int(v.datlen), Cap:int(v.datlen)})))
+	} else {
+		ptr := make([]byte, int(v.datlen))
+		v.data = data_ptr_t(&ptr)
+		f.Seek(int64(v.datpos), os.SEEK_SET)
+		f.Read(ptr)
+	}
 }
