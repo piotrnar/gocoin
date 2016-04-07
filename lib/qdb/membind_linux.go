5bd488f6a405d3d158a058465d6f14b7adef796d
Piotr Narewski <piotrnar@gmail.com>
2016-04-07 12:23:26 +0200

    Former-commit-id: 7833dae8e4e4110441f1bb44a106ddc6507cfe3a Former-commit-id: 3fe874852124cf18378b67b039d8b2b88137886e

---
+++ lib/qdb/membind_linux.go
@@ -0,0 +1,53 @@
+// +build linux
+
+/*
+If this file does not build and you don't know what to do, simply delete it and rebuild.
+*/
+
+package qdb
+
+/*
+#include <stdlib.h>
+#include <string.h>
+
+void *alloc_ptr(void *c, unsigned long l) {
+	void *ptr = malloc(l);
+	memcpy(ptr, c, l);
+	return ptr;
+}
+
+void *my_alloc(unsigned long l) {
+	return malloc(l);
+}
+
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+
+func gcc_HeapAlloc(le uint32) data_ptr_t {
+	return data_ptr_t(C.my_alloc(C.ulong(le)))
+}
+
+func gcc_HeapFree(ptr data_ptr_t) {
+	C.free(unsafe.Pointer(ptr))
+}
+
+func gcc_AllocPtr(v []byte) data_ptr_t {
+	return data_ptr_t(C.alloc_ptr(unsafe.Pointer(&v[0]), C.ulong(len(v))))
+}
+
+func init() {
+	if membind_use_wrapper {
+		panic("Another wrapper already initialized")
+	}
+	println("Using CGO for QDB memory. Remove file qdb/membind_linux.go if you encounter issues.")
+	println("If you use go 1.6, downgrade to 1.5.3 (https://github.com/golang/go/issues/15172).")
+	_HeapAlloc = gcc_HeapAlloc
+	_HeapFree = gcc_HeapFree
+	_AllocPtr = gcc_AllocPtr
+	membind_use_wrapper = true
+}
