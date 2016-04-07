9e87653303334760779e25928d69e28082e4a093
Piotr Narewski <piotrnar@gmail.com>
2016-04-07 12:23:27 +0200

    Former-commit-id: 9e06a1eabc32672a0f481d5a237c35ee27d669cd Former-commit-id: bd53f57b40f853837b414e125721d1466c069267

---
+++ lib/qdb/membind_windows.go
@@ -0,0 +1,51 @@
+// +build windows
+
+package qdb
+
+import (
+	"unsafe"
+	"reflect"
+	"syscall"
+)
+
+var (
+	funcGlobalAlloc *syscall.Proc
+	funcGlobalFree  *syscall.Proc
+)
+
+func win_HeapAlloc(le uint32) data_ptr_t {
+	ptr, _, _ := funcGlobalAlloc.Call(0, uintptr(le))
+	return data_ptr_t(ptr)
+}
+
+func win_HeapFree(ptr data_ptr_t) {
+	funcGlobalFree.Call(uintptr(ptr))
+}
+
+func win_AllocPtr(v []byte) data_ptr_t {
+	ptr := win_HeapAlloc(uint32(len(v)))
+	sl := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data:uintptr(ptr), Len:int(len(v)), Cap:int(len(v))}))
+	copy(sl, v)
+	return ptr
+}
+
+
+func init() {
+	if membind_use_wrapper {
+		return
+	}
+	dll, er := syscall.LoadDLL("kernel32.dll")
+	if er!=nil {
+		return
+	}
+	funcGlobalAlloc, _ = dll.FindProc("GlobalAlloc")
+	funcGlobalFree, _ = dll.FindProc("GlobalFree")
+	if funcGlobalAlloc==nil || funcGlobalFree==nil {
+		return
+	}
+	println("Using Windows API for QDB memory. Remove file qdb/membind_win.go if you encounter issues.")
+	_HeapAlloc = win_HeapAlloc
+	_HeapFree = win_HeapFree
+	_AllocPtr = win_AllocPtr
+	membind_use_wrapper = true
+}
