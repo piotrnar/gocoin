b7abbb79dc94b50123818a49bbc6c0044220d44d
Piotr Narewski <piotrnar@gmail.com>
2016-04-14 14:53:55 +0200

    Former-commit-id: 746476157624c5d4f3c1afb373a0404961c2b4bd Former-commit-id: 2d44d5315f86c5c14f3b578ebfb0b9d8ea75a251

--- lib/test/block_validator/run_validator.bat
+++ lib/test/block_validator/run_validator.bat
@@ -1,10 +1,8 @@
 @echo off
-pskill block_validator
 go build next_block.go
 if errorlevel 1 goto fin
 go build block_validator.go
 if errorlevel 1 goto fin
-start block_validator
-java -jar bitcoin.jar
+echo While the validator is running feed it with the test blocks by executing: java -jar bitcoin.jar
+block_validator.exe
 :fin
-del next_block.exe block_validator.exe dupa.bin
