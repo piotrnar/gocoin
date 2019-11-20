fab94d327df9ded6d99f7f447146bf4d188933f3
Piotr N <piotrnar@gmail.com>
2019-11-20 11:13:38 +0000

    Revert "go fmt on lib/btc/func.go"
    This reverts commit d168b3493c31ba42b7c789eceaaf33c75d4373e3.

--- lib/btc/funcs.go
+++ lib/btc/funcs.go
@@ -1,59 +1,61 @@
 package btc
 
 import (
+	"os"
+	"io"
+	"fmt"
 	"bytes"
-	"crypto/sha256"
+	"errors"
+	"strings"
+	"strconv"
 	"encoding/base64"
 	"encoding/binary"
-	"errors"
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
+	"crypto/sha256"
 )
 
 func allzeros(b []byte) bool {
 	for i := range b {
-		if b[i] != 0 {
+		if b[i]!=0 {
 			return false
 		}
 	}
 	return true
 }
 
+
 // Puts var_length field into the given buffer
 func PutVlen(b []byte, vl int) uint32 {
 	uvl := uint32(vl)
-	if uvl < 0xfd {
+	if uvl<0xfd {
 		b[0] = byte(uvl)
 		return 1
 	}
-	if uvl < 0x10000 {
+	if uvl<0x10000 {
 		b[0] = 0xfd
 		b[1] = byte(uvl)
-		b[2] = byte(uvl >> 8)
+		b[2] = byte(uvl>>8)
 		return 3
 	}
 	b[0] = 0xfe
 	b[1] = byte(uvl)
-	b[2] = byte(uvl >> 8)
-	b[3] = byte(uvl >> 16)
-	b[4] = byte(uvl >> 24)
+	b[2] = byte(uvl>>8)
+	b[3] = byte(uvl>>16)
+	b[4] = byte(uvl>>24)
 	return 5
 }
 
+
 func PutULe(b []byte, uvl uint64) int {
-	if uvl < 0xfd {
+	if uvl<0xfd {
 		b[0] = byte(uvl)
 		return 1
 	}
-	if uvl < 0x10000 {
+	if uvl<0x10000 {
 		b[0] = 0xfd
 		binary.LittleEndian.PutUint16(b[1:3], uint16(uvl))
 		return 3
 	}
-	if uvl < 0x100000000 {
+	if uvl<0x100000000 {
 		b[0] = 0xfe
 		binary.LittleEndian.PutUint32(b[1:5], uint32(uvl))
 		return 5
@@ -63,78 +65,82 @@ func PutULe(b []byte, uvl uint64) int {
 	return 9
 }
 
+
 // How many bytes would take to write this VLen
 func VLenSize(uvl uint64) int {
-	if uvl < 0xfd {
+	if uvl<0xfd {
 		return 1
 	}
-	if uvl < 0x10000 {
+	if uvl<0x10000 {
 		return 3
 	}
-	if uvl < 0x100000000 {
+	if uvl<0x100000000 {
 		return 5
 	}
 	return 9
 }
+
 
 // Returns var_int and number of bytes that the var_int took
 // If there is not enough bytes in the buffer, it return 0, 0
 func VLen(b []byte) (le int, var_int_siz int) {
 	if len(b) > 0 {
 		switch b[0] {
-		case 0xfd:
-			if len(b) >= 3 {
-				return int(binary.LittleEndian.Uint16(b[1:3])), 3
-			}
-		case 0xfe:
-			if len(b) >= 5 {
-				return int(binary.LittleEndian.Uint32(b[1:5])), 5
-			}
-		case 0xff:
-			if len(b) >= 9 {
-				return int(binary.LittleEndian.Uint64(b[1:9])), 9
-			}
-		default:
-			return int(b[0]), 1
+			case 0xfd:
+				if len(b) >= 3 {
+					return int(binary.LittleEndian.Uint16(b[1:3])), 3
+				}
+			case 0xfe:
+				if len(b) >= 5 {
+					return int(binary.LittleEndian.Uint32(b[1:5])), 5
+				}
+			case 0xff:
+				if len(b) >= 9 {
+					return int(binary.LittleEndian.Uint64(b[1:9])), 9
+				}
+			default:
+				return int(b[0]), 1
 		}
 	}
 	return
 }
+
 
 // Returns var_uint and number of bytes that the var_uint took
 // If there is not enough bytes in the buffer, it return 0, 0
 func VULe(b []byte) (le uint64, var_int_siz int) {
 	if len(b) > 0 {
 		switch b[0] {
-		case 0xfd:
-			if len(b) >= 3 {
-				return uint64(binary.LittleEndian.Uint16(b[1:3])), 3
-			}
-		case 0xfe:
-			if len(b) >= 5 {
-				return uint64(binary.LittleEndian.Uint32(b[1:5])), 5
-			}
-		case 0xff:
-			if len(b) >= 9 {
-				return uint64(binary.LittleEndian.Uint64(b[1:9])), 9
-			}
-		default:
-			return uint64(b[0]), 1
+			case 0xfd:
+				if len(b) >= 3 {
+					return uint64(binary.LittleEndian.Uint16(b[1:3])), 3
+				}
+			case 0xfe:
+				if len(b) >= 5 {
+					return uint64(binary.LittleEndian.Uint32(b[1:5])), 5
+				}
+			case 0xff:
+				if len(b) >= 9 {
+					return uint64(binary.LittleEndian.Uint64(b[1:9])), 9
+				}
+			default:
+				return uint64(b[0]), 1
 		}
 	}
 	return
 }
 
+
 func CalcMerkle(mtr [][32]byte) (res []byte, mutated bool) {
 	var j, i2 int
-	for siz := len(mtr); siz > 1; siz = (siz + 1) / 2 {
+	for siz:=len(mtr); siz>1; siz=(siz+1)/2 {
 		for i := 0; i < siz; i += 2 {
 			if i+1 < siz-1 {
-				i2 = i + 1
+				i2 = i+1
 			} else {
-				i2 = siz - 1
+				i2 = siz-1
 			}
-			if i != i2 && bytes.Equal(mtr[j+i][:], mtr[j+i2][:]) {
+			if i!=i2 && bytes.Equal(mtr[j+i][:], mtr[j+i2][:]) {
 				mutated = true
 			}
 			s := sha256.New()
@@ -154,21 +160,23 @@ func CalcMerkle(mtr [][32]byte) (res []byte, mutated bool) {
 	return
 }
 
+
 func GetWitnessMerkle(txs []*Tx) (res []byte, mutated bool) {
 	mtr := make([][32]byte, len(txs), 3*len(txs)) // make the buffer 3 times longer as we use append() inside CalcMerkle
 	//mtr[0] = make([]byte, 32) // null
-	for i := 1; i < len(txs); i++ {
+	for i:=1; i<len(txs); i++ {
 		mtr[i] = txs[i].WTxID().Hash
 	}
 	res, mutated = CalcMerkle(mtr)
 	return
 }
 
+
 func ReadAll(rd io.Reader, b []byte) (er error) {
 	var n int
-	for i := 0; i < len(b); i += n {
+	for i:=0; i<len(b); i+=n {
 		n, er = rd.Read(b[i:])
-		if er != nil {
+		if er!=nil {
 			return
 		}
 	}
@@ -177,7 +185,7 @@ func ReadAll(rd io.Reader, b []byte) (er error) {
 
 // Reads var_len from the given reader
 func ReadVLen(b io.Reader) (res uint64, e error) {
-	var buf [8]byte
+	var buf [8]byte;
 
 	if e = ReadAll(b, buf[:1]); e != nil {
 		//println("ReadVLen1 error:", e.Error())
@@ -189,13 +197,14 @@ func ReadVLen(b io.Reader) (res uint64, e error) {
 		return
 	}
 
-	c := 2 << (2 - (0xff - buf[0]))
+	c := 2 << (2-(0xff-buf[0]));
+
 
 	if e = ReadAll(b, buf[:c]); e != nil {
 		println("ReadVLen1 error:", e.Error())
 		return
 	}
-	for i := 0; i < c; i++ {
+	for i:=0; i<c; i++ {
 		res |= (uint64(buf[i]) << uint64(8*i))
 	}
 	return
@@ -224,21 +233,22 @@ func WriteVlen(b io.Writer, var_len uint64) {
 // Writes opcode to put a specific number of bytes to stack
 func WritePutLen(b io.Writer, data_len uint32) {
 	switch {
-	case data_len <= OP_PUSHDATA1:
-		b.Write([]byte{byte(data_len)})
+		case data_len <= OP_PUSHDATA1:
+			b.Write([]byte{byte(data_len)})
 
-	case data_len < 0x100:
-		b.Write([]byte{OP_PUSHDATA1, byte(data_len)})
+		case data_len < 0x100:
+			b.Write([]byte{OP_PUSHDATA1,byte(data_len)})
 
-	case data_len < 0x10000:
-		b.Write([]byte{OP_PUSHDATA2})
-		binary.Write(b, binary.LittleEndian, uint16(data_len))
+		case data_len < 0x10000:
+			b.Write([]byte{OP_PUSHDATA2})
+			binary.Write(b, binary.LittleEndian, uint16(data_len))
 
-	default:
-		b.Write([]byte{OP_PUSHDATA4})
-		binary.Write(b, binary.LittleEndian, uint32(data_len))
+		default:
+			b.Write([]byte{OP_PUSHDATA4})
+			binary.Write(b, binary.LittleEndian, uint32(data_len))
 	}
 }
+
 
 // Read bitcoin protocol string
 func ReadString(rd io.Reader) (s string, e error) {
@@ -255,6 +265,7 @@ func ReadString(rd io.Reader) (s string, e error) {
 	return
 }
 
+
 // Takes a base64 encoded bitcoin generated signature and decodes it
 func ParseMessageSignature(encsig string) (nv byte, sig *Signature, er error) {
 	var sd []byte
@@ -264,7 +275,7 @@ func ParseMessageSignature(encsig string) (nv byte, sig *Signature, er error) {
 		return
 	}
 
-	if len(sd) != 65 {
+	if len(sd)!=65 {
 		er = errors.New("The decoded signature is not 65 bytes long")
 		return
 	}
@@ -275,15 +286,16 @@ func ParseMessageSignature(encsig string) (nv byte, sig *Signature, er error) {
 	sig.R.SetBytes(sd[1:33])
 	sig.S.SetBytes(sd[33:65])
 
-	if nv < 27 || nv > 34 {
+	if nv<27 || nv>34 {
 		er = errors.New("nv out of range")
 	}
 
 	return
 }
 
+
 func IsPayToScript(scr []byte) bool {
-	return len(scr) == 23 && scr[0] == OP_HASH160 && scr[1] == 0x14 && scr[22] == OP_EQUAL
+	return len(scr)==23 && scr[0]==OP_HASH160 && scr[1]==0x14 && scr[22]==OP_EQUAL
 }
 
 // Parses a floating number string to return uint64 value expressed in Satoshi's
@@ -292,7 +304,7 @@ func StringToSatoshis(s string) (val uint64, er error) {
 	var big, small uint64
 
 	ss := strings.Split(s, ".")
-	if len(ss) == 1 {
+	if len(ss)==1 {
 		val, er = strconv.ParseUint(ss[0], 10, 64)
 		if er != nil {
 			return
@@ -300,16 +312,16 @@ func StringToSatoshis(s string) (val uint64, er error) {
 		val *= 1e8
 		return
 	}
-	if len(ss) != 2 {
+	if len(ss)!=2 {
 		println("Incorrect amount", s)
 		os.Exit(1)
 	}
 
-	if len(ss[1]) > 8 {
+	if len(ss[1])>8 {
 		er = errors.New("Too many decimal points")
 		return
 	}
-	if len(ss[1]) < 8 {
+	if len(ss[1])<8 {
 		ss[1] += strings.Repeat("0", 8-len(ss[1]))
 	}
 
@@ -327,25 +339,26 @@ func StringToSatoshis(s string) (val uint64, er error) {
 }
 
 // Converts value of satoshis to a BTC-value string (with 8 decimal points)
-func UintToBtc(val uint64) string {
+func UintToBtc(val uint64) (string) {
 	return fmt.Sprintf("%d.%08d", val/1e8, val%1e8)
 }
 
 // Return true if the given PK_script is a standard P2SH
 func IsP2SH(d []byte) bool {
-	return len(d) == 23 && d[0] == 0xa9 && d[1] == 20 && d[22] == 0x87
+	return len(d)==23 && d[0]==0xa9 && d[1]==20 && d[22]==0x87
 }
 
 // Returns true if the given PK_script is anyhow usefull to gocoin's node
 func IsUsefullOutScript(v []byte) bool {
-	if len(v) == 25 && v[0] == 0x76 && v[1] == 0xa9 && v[2] == 0x14 && v[23] == 0x88 && v[24] == 0xac {
+	if len(v)==25 && v[0]==0x76 && v[1]==0xa9 && v[2]==0x14 && v[23]==0x88 && v[24]==0xac {
 		return true // P2KH
 	}
-	if len(v) == 23 && v[0] == 0xa9 && v[1] == 0x14 && v[22] == 0x87 {
+	if len(v)==23 && v[0]==0xa9 && v[1]==0x14 && v[22]==0x87 {
 		return true // P2SH
 	}
 	return false
 }
+
 
 func GetOpcode(b []byte) (opcode int, ret []byte, pc int, e error) {
 	// Read instruction
@@ -373,21 +386,21 @@ func GetOpcode(b []byte) (opcode int, ret []byte, pc int, e error) {
 				e = errors.New("GetOpcode error 3")
 				return
 			}
-			size = int(binary.LittleEndian.Uint16(b[pc : pc+2]))
+			size = int(binary.LittleEndian.Uint16(b[pc:pc+2]))
 			pc += 2
 		} else if opcode == OP_PUSHDATA4 {
 			if pc+4 > len(b) {
 				e = errors.New("GetOpcode error 4")
 				return
 			}
-			size = int(binary.LittleEndian.Uint16(b[pc : pc+4]))
+			size = int(binary.LittleEndian.Uint16(b[pc:pc+4]))
 			pc += 4
 		}
 		if pc+size > len(b) {
 			e = errors.New(fmt.Sprint("GetOpcode size to fetch exceeds remainig data left: ", pc+size, "/", len(b)))
 			return
 		}
-		ret = b[pc : pc+size]
+		ret = b[pc:pc+size]
 		pc += size
 	}
 
@@ -403,10 +416,10 @@ func GetSigOpCount(scr []byte, fAccurate bool) (n uint) {
 			break
 		}
 		pc += le
-		if opcode == 0xac /*OP_CHECKSIG*/ || opcode == 0xad /*OP_CHECKSIGVERIFY*/ {
+		if opcode == 0xac/*OP_CHECKSIG*/ || opcode == 0xad/*OP_CHECKSIGVERIFY*/ {
 			n++
-		} else if opcode == 0xae /*OP_CHECKMULTISIG*/ || opcode == 0xaf /*OP_CHECKMULTISIGVERIFY*/ {
-			if fAccurate && lastOpcode >= 0x51 /*OP_1*/ && lastOpcode <= 0x60 /*OP_16*/ {
+		} else if opcode == 0xae/*OP_CHECKMULTISIG*/ || opcode == 0xaf/*OP_CHECKMULTISIGVERIFY*/ {
+			if fAccurate && lastOpcode >= 0x51/*OP_1*/ && lastOpcode <= 0x60/*OP_16*/ {
 				n += uint(DecodeOP_N(lastOpcode))
 			} else {
 				n += MAX_PUBKEYS_PER_MULTISIG
@@ -418,11 +431,12 @@ func GetSigOpCount(scr []byte, fAccurate bool) (n uint) {
 }
 
 func DecodeOP_N(opcode byte) int {
-	if opcode == 0x00 /*OP_0*/ {
+	if opcode == 0x00/*OP_0*/ {
 		return 0
 	}
-	return int(opcode) - 0x50 /*OP_1-1*/
+	return int(opcode) - 0x50/*OP_1-1*/
 }
+
 
 func GetP2SHSigOpCount(scr []byte) uint {
 	// This is a pay-to-script-hash scriptPubKey;
@@ -437,7 +451,7 @@ func GetP2SHSigOpCount(scr []byte) uint {
 			return 0
 		}
 		pc += le
-		if opcode > 0x60 /*OP_16*/ {
+		if opcode > 0x60/*OP_16*/ {
 			return 0
 		}
 	}
@@ -449,10 +463,10 @@ func IsWitnessProgram(scr []byte) (version int, program []byte) {
 	if len(scr) < 4 || len(scr) > 42 {
 		return
 	}
-	if scr[0] != OP_0 && (scr[0] < OP_1 || scr[0] > OP_16) {
+	if scr[0]!=OP_0 && (scr[0]<OP_1 || scr[0]>OP_16) {
 		return
 	}
-	if int(scr[1])+2 == len(scr) {
+	if int(scr[1]) + 2 == len(scr) {
 		version = DecodeOP_N(scr[0])
 		program = scr[2:]
 	}
@@ -461,21 +475,21 @@ func IsWitnessProgram(scr []byte) (version int, program []byte) {
 
 func WitnessSigOps(witversion int, witprogram []byte, witness [][]byte) uint {
 	if witversion == 0 {
-		if len(witprogram) == 20 {
+		if len(witprogram)==20 {
 			return 1
 		}
 
-		if len(witprogram) == 32 && len(witness) > 0 {
+		if len(witprogram)==32 && len(witness) > 0 {
 			subscript := witness[len(witness)-1]
 			return GetSigOpCount(subscript, true)
 		}
 	}
-	return 0
+    return 0;
 }
 
 func IsPushOnly(scr []byte) bool {
 	idx := 0
-	for idx < len(scr) {
+	for idx<len(scr) {
 		op, _, n, e := GetOpcode(scr[idx:])
 		if e != nil {
 			return false
