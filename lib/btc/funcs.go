3b91320f176f420bd52b441f83e892079a4c6c04
Piotr N <piotr@gocoin.pl>
2021-05-09 14:51:55 +0200

    Fix for testnet block #1971687 -> Txs[30] (too many sigops)

--- lib/btc/funcs.go
+++ lib/btc/funcs.go
@@ -388,7 +388,7 @@ func GetSigOpCount(scr []byte, fAccurate bool) (n uint) {
 	var lastOpcode byte = 0xff
 	for pc < len(scr) {
 		opcode, _, le, e := GetOpcode(scr[pc:])
-		if e != nil {
+		if e != nil || opcode == 0x6a /*Fix for testnet block #1971687 -> Txs[30] (too many sigops) */ {
 			break
 		}
 		pc += le
