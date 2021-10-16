a34d5bb12f628f2d930b75d27427c3752823b5b4
Piotr N <piotr@gocoin.pl>
2021-10-16 12:43:43 +0200

    Added support for RSK Merged Mining

--- client/common/mining.go
+++ client/common/mining.go
@@ -1,29 +1,36 @@
 package common
 
 import (
-	"sync"
 	"bytes"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"sync"
+
 	"github.com/piotrnar/gocoin/lib/btc"
 )
 
 type oneMinerId struct {
 	Name string
-	Tag []byte
+	Tag  []byte
 }
 
 var MinerIds []oneMinerId
-
 
 // return miner ID of the given coinbase transaction
 func TxMiner(cbtx *btc.Tx) (string, int) {
 	txdat := cbtx.Serialize()
 	for i, m := range MinerIds {
-		if bytes.Equal(m.Tag, []byte("_p2pool_")) {  // P2Pool
-			if len(cbtx.TxOut)>10 &&
-				bytes.Equal(cbtx.TxOut[len(cbtx.TxOut)-1].Pk_script[:2], []byte{0x6A,0x28}) {
+		if bytes.Equal(m.Tag, []byte("_p2pool_")) { // P2Pool
+			if len(cbtx.TxOut) > 10 &&
+				bytes.Equal(cbtx.TxOut[len(cbtx.TxOut)-1].Pk_script[:2], []byte{0x6A, 0x28}) {
 				return m.Name, i
+			}
+		} else if bytes.Equal(m.Tag, []byte("_RSK_")) { // RSK Pool
+			if len(cbtx.TxOut) > 1 {
+				txo := cbtx.TxOut[len(cbtx.TxOut)-1]
+				if len(txo.Pk_script) >= 10 && txo.Pk_script[0] == 0x6A && string(txo.Pk_script[2:10]) == "RSKBLOCK" {
+					return m.Name, i
+				}
 			}
 		} else if bytes.Contains(txdat, m.Tag) {
 			return m.Name, i
@@ -32,7 +39,7 @@ func TxMiner(cbtx *btc.Tx) (string, int) {
 
 	for _, txo := range cbtx.TxOut {
 		adr := btc.NewAddrFromPkScript(txo.Pk_script, Testnet)
-		if adr!=nil {
+		if adr != nil {
 			return adr.String(), -1
 		}
 	}
@@ -40,10 +47,9 @@ func TxMiner(cbtx *btc.Tx) (string, int) {
 	return "", -1
 }
 
-
 func ReloadMiners() {
 	d, _ := ioutil.ReadFile("miners.json")
-	if d!=nil {
+	if d != nil {
 		var MinerIdFile [][3]string
 		e := json.Unmarshal(d, &MinerIdFile)
 		if e != nil {
@@ -54,7 +60,7 @@ func ReloadMiners() {
 		for _, r := range MinerIdFile {
 			var rec oneMinerId
 			rec.Name = r[0]
-			if r[1]!="" {
+			if r[1] != "" {
 				rec.Tag = []byte(r[1])
 			} else {
 				if a, _ := btc.NewAddrFromString(r[2]); a != nil {
@@ -69,18 +75,16 @@ func ReloadMiners() {
 	}
 }
 
-
 var (
-	AverageFeeMutex sync.Mutex
-	AverageFeeBytes uint64
-	AverageFeeTotal uint64
-	AverageFee_SPB float64
+	AverageFeeMutex     sync.Mutex
+	AverageFeeBytes     uint64
+	AverageFeeTotal     uint64
+	AverageFee_SPB      float64
 	averageFeeLastBlock uint32 = 0xffffffff
-	averageFeeLastCount uint = 0xffffffff
+	averageFeeLastCount uint   = 0xffffffff
 )
 
-
-func GetAverageFee() (float64) {
+func GetAverageFee() float64 {
 	Last.Mutex.Lock()
 	end := Last.Block
 	Last.Mutex.Unlock()
@@ -88,7 +92,7 @@ func GetAverageFee() (float64) {
 	LockCfg()
 	blocks := CFG.Stat.FeesBlks
 	UnlockCfg()
-	if blocks<=0 {
+	if blocks <= 0 {
 		blocks = 1 // at leats one block
 	}
 
@@ -111,7 +115,7 @@ func GetAverageFee() (float64) {
 			return 0
 		}
 		block, e := btc.NewBlock(bl)
-		if e!=nil {
+		if e != nil {
 			return 0
 		}
 
@@ -126,18 +130,18 @@ func GetAverageFee() (float64) {
 			AverageFeeTotal += uint64(fees_from_this_block)
 		}
 
-		AverageFeeBytes += uint64(len(bl)-block.TxOffset-cbasetxlen) /*do not count block header and conibase tx */
+		AverageFeeBytes += uint64(len(bl) - block.TxOffset - cbasetxlen) /*do not count block header and conibase tx */
 
 		blocks--
 		end = end.Parent
 	}
-	if AverageFeeBytes==0 {
-		if AverageFeeTotal!=0 {
+	if AverageFeeBytes == 0 {
+		if AverageFeeTotal != 0 {
 			panic("Impossible that miner gest a fee with no transactions in the block")
 		}
 		AverageFee_SPB = 0
 	} else {
-		AverageFee_SPB = float64(AverageFeeTotal)/float64(AverageFeeBytes)
+		AverageFee_SPB = float64(AverageFeeTotal) / float64(AverageFeeBytes)
 	}
 	return AverageFee_SPB
 }
