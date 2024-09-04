d97edeed801b58338a4a6d861ea218a63dca7814
Nithish Karthik <nithishkarthik01@gmail.com>
2024-09-04 15:46:49 +0530

    refactor: create make_tx func

--- client/usif/webui/sendtx.go
+++ client/usif/webui/sendtx.go
@@ -7,6 +7,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
@@ -34,8 +35,145 @@ type MultisigAddr struct {
 	ListOfAddres               []string
 }
 
+func make_tx(form url.Values, tx *btc.Tx) (err string, thisbal utxo.AllUnspentTx, pay_cmd string){
+    var totalinput, spentsofar uint64
+    var change_addr *btc.BtcAddr
 
-// TODO: Get rid of duplication between this and the dl_payment function
+    tx.Version = 1
+    tx.Lock_time = 0
+
+    seq, er := strconv.ParseInt(form["tx_seq"][0], 10, 64)
+    if er != nil || seq < -1000000 || seq > 0xffffffff {
+        err = "Incorrect Sequence value: " + form["tx_seq"][0]
+        return err, nil, ""
+    }
+
+    outcnt, _ := strconv.ParseUint(form["outcnt"][0], 10, 32)
+
+    lck := new(usif.OneLock)
+    lck.In.Add(1)
+    lck.Out.Add(1)
+    usif.LocksChan <- lck
+    lck.In.Wait()
+    defer lck.Out.Done()
+
+    for i := 1; i <= int(outcnt); i++ {
+        is := fmt.Sprint(i)
+        if len(form["txout"+is]) == 1 && form["txout"+is][0] == "on" {
+            hash := btc.NewUint256FromString(form["txid"+is][0])
+            if hash != nil {
+                vout, er := strconv.ParseUint(form["txvout"+is][0], 10, 32)
+                if er == nil {
+                    var po = btc.TxPrevOut{Hash: hash.Hash, Vout: uint32(vout)}
+                    if res := common.BlockChain.Unspent.UnspentGet(&po); res != nil {
+                        addr := btc.NewAddrFromPkScript(res.Pk_script, common.Testnet)
+
+                        unsp := &utxo.OneUnspentTx{TxPrevOut: po, Value: res.Value,
+                            MinedAt: res.BlockHeight, Coinbase: res.WasCoinbase, BtcAddr: addr}
+
+                        thisbal = append(thisbal, unsp)
+
+                        // Add the input to our tx
+                        tin := new(btc.TxIn)
+                        tin.Input = po
+                        tin.Sequence = uint32(seq)
+                        tx.TxIn = append(tx.TxIn, tin)
+
+                        // Add the value to total input value
+                        totalinput += res.Value
+
+                        // If no change specified, use the first input addr as it
+                        if change_addr == nil {
+                            change_addr = addr
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if change_addr == nil {
+        // There werte no inputs
+        return
+    }
+
+    for i := 1; ; i++ {
+        adridx := fmt.Sprint("adr", i)
+        btcidx := fmt.Sprint("btc", i)
+
+        if len(form[adridx]) != 1 || len(form[btcidx]) != 1 {
+            break
+        }
+
+        if len(form[adridx][0]) > 1 {
+            addr, er := btc.NewAddrFromString(form[adridx][0])
+            if er == nil {
+                am, er := btc.StringToSatoshis(form[btcidx][0])
+                if er == nil && am > 0 {
+                    if pay_cmd == "" {
+                        pay_cmd = "wallet -a=false -useallinputs -send "
+                    } else {
+                        pay_cmd += ","
+                    }
+                    pay_cmd += addr.String() + "=" + btc.UintToBtc(am)
+
+                    outs, er := btc.NewSpendOutputs(addr, am, common.CFG.Testnet)
+                    if er != nil {
+                        err = er.Error()
+                        return err, nil, ""
+                    }
+                    tx.TxOut = append(tx.TxOut, outs...)
+
+                    spentsofar += am
+                } else {
+                    err = "Incorrect amount (" + form[btcidx][0] + ") for Output #" + fmt.Sprint(i)
+                    return err, nil, ""
+                }
+            } else {
+                err = "Incorrect address (" + form[adridx][0] + ") for Output #" + fmt.Sprint(i)
+                return err, nil, ""
+            }
+        }
+    }
+
+    if pay_cmd == "" {
+        err = "No inputs selected"
+        return err, nil, ""
+    }
+
+    pay_cmd += fmt.Sprint(" -seq ", seq)
+
+    am, er := btc.StringToSatoshis(form["txfee"][0])
+    if er != nil {
+        err = "Incorrect fee value: " + form["txfee"][0]
+        return err, nil, ""
+    }
+
+    pay_cmd += " -fee " + form["txfee"][0]
+    spentsofar += am
+
+    if len(form["change"][0]) > 1 {
+        addr, er := btc.NewAddrFromString(form["change"][0])
+        if er != nil {
+            err = "Incorrect change address: " + form["change"][0]
+            return err, nil, ""
+        }
+        change_addr = addr
+    }
+    pay_cmd += " -change " + change_addr.String()
+
+    if totalinput > spentsofar {
+        // Add change output
+        outs, er := btc.NewSpendOutputs(change_addr, totalinput-spentsofar, common.CFG.Testnet)
+        if er != nil {
+            err = er.Error()
+            return err, nil, ""
+        }
+        tx.TxOut = append(tx.TxOut, outs...)
+    }
+    return "", thisbal, pay_cmd
+}
+
 func sign_transaction(wrs *rmtsrv.WebsocketServer)func (w http.ResponseWriter, r *http.Request) {
     return func(w http.ResponseWriter, r *http.Request) {
         fmt.Println("Received sign transaction request here")
@@ -51,146 +189,12 @@ func sign_transaction(wrs *rmtsrv.WebsocketServer)func (w http.ResponseWriter, r
                 goto error
         }
 
-        // here rather than downloading the zip file, we have to send it as a json to the client server
         if len(r.Form["outcnt"])==1 {
-            var thisbal utxo.AllUnspentTx
-            var pay_cmd string
-            var totalinput, spentsofar uint64
-            var change_addr *btc.BtcAddr
-
             tx := new(btc.Tx)
-            tx.Version = 1
-            tx.Lock_time = 0
-
-            seq, er := strconv.ParseInt(r.Form["tx_seq"][0], 10, 64)
-            if er != nil || seq < -2 || seq > 0xffffffff {
-                err = "Incorrect Sequence value: " + r.Form["tx_seq"][0]
+            er, thisbal, pay_cmd := make_tx(r.Form, tx)
+            if err != "" {
+                err = er
                 goto error
-            }
-
-            outcnt, _ := strconv.ParseUint(r.Form["outcnt"][0], 10, 32)
-
-            lck := new(usif.OneLock)
-            lck.In.Add(1)
-            lck.Out.Add(1)
-            usif.LocksChan <- lck
-            lck.In.Wait()
-            defer lck.Out.Done()
-
-            for i:=1; i<=int(outcnt); i++ {
-                is := fmt.Sprint(i)
-                if len(r.Form["txout"+is])==1 && r.Form["txout"+is][0]=="on" {
-                    hash := btc.NewUint256FromString(r.Form["txid"+is][0])
-                    if hash!=nil {
-                        vout, er := strconv.ParseUint(r.Form["txvout"+is][0], 10, 32)
-                        if er==nil {
-                            var po = btc.TxPrevOut{Hash:hash.Hash, Vout:uint32(vout)}
-                            if res := common.BlockChain.Unspent.UnspentGet(&po); res != nil {
-                                addr := btc.NewAddrFromPkScript(res.Pk_script, common.Testnet)
-
-                                unsp := &utxo.OneUnspentTx{TxPrevOut:po, Value:res.Value,
-                                    MinedAt:res.BlockHeight, Coinbase:res.WasCoinbase, BtcAddr:addr}
-
-                                thisbal = append(thisbal, unsp)
-
-                                // Add the input to our tx
-                                tin := new(btc.TxIn)
-                                tin.Input = po
-                                tin.Sequence = uint32(seq)
-                                tx.TxIn = append(tx.TxIn, tin)
-
-                                // Add the value to total input value
-                                totalinput += res.Value
-
-                                // If no change specified, use the first input addr as it
-                                if change_addr == nil {
-                                    change_addr = addr
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            if change_addr == nil {
-                // There werte no inputs
-                return
-            }
-
-            for i:=1; ; i++ {
-                adridx := fmt.Sprint("adr", i)
-                btcidx := fmt.Sprint("btc", i)
-
-                if len(r.Form[adridx])!=1 || len(r.Form[btcidx])!=1 {
-                    break
-                }
-
-                if len(r.Form[adridx][0])>1 {
-                    addr, er := btc.NewAddrFromString(r.Form[adridx][0])
-                    if er == nil {
-                        am, er := btc.StringToSatoshis(r.Form[btcidx][0])
-                        if er==nil && am>0 {
-                            if pay_cmd=="" {
-                                pay_cmd = "wallet -a=false -useallinputs -send "
-                            } else {
-                                pay_cmd += ","
-                            }
-                            pay_cmd += addr.String() + "=" + btc.UintToBtc(am)
-
-                            outs, er := btc.NewSpendOutputs(addr, am, common.CFG.Testnet)
-                            if er != nil {
-                                err = er.Error()
-                                goto error
-                            }
-                            tx.TxOut = append(tx.TxOut, outs...)
-
-                            spentsofar += am
-                        } else {
-                            err = "Incorrect amount (" + r.Form[btcidx][0] + ") for Output #" + fmt.Sprint(i)
-                            goto error
-                        }
-                    } else {
-                        err = "Incorrect address (" + r.Form[adridx][0] + ") for Output #" + fmt.Sprint(i)
-                        goto error
-                    }
-                }
-            }
-
-            if pay_cmd=="" {
-                err = "No inputs selected"
-                goto error
-            }
-
-
-            pay_cmd += fmt.Sprint(" -seq ", seq)
-
-            am, er := btc.StringToSatoshis(r.Form["txfee"][0])
-            if er != nil {
-                err = "Incorrect fee value: " + r.Form["txfee"][0]
-                goto error
-            }
-
-            pay_cmd += " -fee " + r.Form["txfee"][0]
-            spentsofar += am
-
-            if len(r.Form["change"][0])>1 {
-                addr, er := btc.NewAddrFromString(r.Form["change"][0])
-                if er != nil {
-                    err = "Incorrect change address: " + r.Form["change"][0]
-                    goto error
-                }
-                change_addr = addr
-            }
-            pay_cmd += " -change " + change_addr.String()
-
-            if totalinput > spentsofar {
-                // Add change output
-                outs, er := btc.NewSpendOutputs(change_addr, totalinput - spentsofar, common.CFG.Testnet)
-                if er != nil {
-                    err = er.Error()
-                    goto error
-                }
-                tx.TxOut = append(tx.TxOut, outs...)
             }
 
             st := rmtcmn.SignTransactionRequestPayload{}
@@ -239,7 +243,7 @@ func sign_transaction(wrs *rmtsrv.WebsocketServer)func (w http.ResponseWriter, r
         w.WriteHeader(http.StatusInternalServerError)
         w.Write([]byte(err))
         return 
-}
+    }
 }
 
 func dl_payment(w http.ResponseWriter, r *http.Request) {
@@ -250,148 +254,14 @@ func dl_payment(w http.ResponseWriter, r *http.Request) {
 	var err string
 
 	if len(r.Form["outcnt"]) == 1 {
-		var thisbal utxo.AllUnspentTx
-		var pay_cmd string
-		var totalinput, spentsofar uint64
-		var change_addr *btc.BtcAddr
+        tx := new(btc.Tx)
+        err, thisbal, pay_cmd := make_tx(r.Form, tx)
+        if err != "" {
+            goto error
+        }
 
-		tx := new(btc.Tx)
-		tx.Version = 1
-		tx.Lock_time = 0
-
-		seq, er := strconv.ParseInt(r.Form["tx_seq"][0], 10, 64)
-		if er != nil || seq < -1000000 || seq > 0xffffffff {
-			err = "Incorrect Sequence value: " + r.Form["tx_seq"][0]
-			goto error
-		}
-
-		outcnt, _ := strconv.ParseUint(r.Form["outcnt"][0], 10, 32)
-
-		lck := new(usif.OneLock)
-		lck.In.Add(1)
-		lck.Out.Add(1)
-		usif.LocksChan <- lck
-		lck.In.Wait()
-		defer lck.Out.Done()
-
-		for i := 1; i <= int(outcnt); i++ {
-			is := fmt.Sprint(i)
-			if len(r.Form["txout"+is]) == 1 && r.Form["txout"+is][0] == "on" {
-				hash := btc.NewUint256FromString(r.Form["txid"+is][0])
-				if hash != nil {
-					vout, er := strconv.ParseUint(r.Form["txvout"+is][0], 10, 32)
-					if er == nil {
-						var po = btc.TxPrevOut{Hash: hash.Hash, Vout: uint32(vout)}
-						if res := common.BlockChain.Unspent.UnspentGet(&po); res != nil {
-							addr := btc.NewAddrFromPkScript(res.Pk_script, common.Testnet)
-
-							unsp := &utxo.OneUnspentTx{TxPrevOut: po, Value: res.Value,
-								MinedAt: res.BlockHeight, Coinbase: res.WasCoinbase, BtcAddr: addr}
-
-							thisbal = append(thisbal, unsp)
-
-							// Add the input to our tx
-							tin := new(btc.TxIn)
-							tin.Input = po
-							tin.Sequence = uint32(seq)
-							tx.TxIn = append(tx.TxIn, tin)
-
-							// Add the value to total input value
-							totalinput += res.Value
-
-							// If no change specified, use the first input addr as it
-							if change_addr == nil {
-								change_addr = addr
-							}
-						}
-					}
-				}
-			}
-		}
-
-		if change_addr == nil {
-			// There werte no inputs
-			return
-		}
-
-		for i := 1; ; i++ {
-			adridx := fmt.Sprint("adr", i)
-			btcidx := fmt.Sprint("btc", i)
-
-			if len(r.Form[adridx]) != 1 || len(r.Form[btcidx]) != 1 {
-				break
-			}
-
-			if len(r.Form[adridx][0]) > 1 {
-				addr, er := btc.NewAddrFromString(r.Form[adridx][0])
-				if er == nil {
-					am, er := btc.StringToSatoshis(r.Form[btcidx][0])
-					if er == nil && am > 0 {
-						if pay_cmd == "" {
-							pay_cmd = "wallet -a=false -useallinputs -send "
-						} else {
-							pay_cmd += ","
-						}
-						pay_cmd += addr.String() + "=" + btc.UintToBtc(am)
-
-						outs, er := btc.NewSpendOutputs(addr, am, common.CFG.Testnet)
-						if er != nil {
-							err = er.Error()
-							goto error
-						}
-						tx.TxOut = append(tx.TxOut, outs...)
-
-						spentsofar += am
-					} else {
-						err = "Incorrect amount (" + r.Form[btcidx][0] + ") for Output #" + fmt.Sprint(i)
-						goto error
-					}
-				} else {
-					err = "Incorrect address (" + r.Form[adridx][0] + ") for Output #" + fmt.Sprint(i)
-					goto error
-				}
-			}
-		}
-
-		if pay_cmd == "" {
-			err = "No inputs selected"
-			goto error
-		}
-
-		pay_cmd += fmt.Sprint(" -seq ", seq)
-
-		am, er := btc.StringToSatoshis(r.Form["txfee"][0])
-		if er != nil {
-			err = "Incorrect fee value: " + r.Form["txfee"][0]
-			goto error
-		}
-
-		pay_cmd += " -fee " + r.Form["txfee"][0]
-		spentsofar += am
-
-		if len(r.Form["change"][0]) > 1 {
-			addr, er := btc.NewAddrFromString(r.Form["change"][0])
-			if er != nil {
-				err = "Incorrect change address: " + r.Form["change"][0]
-				goto error
-			}
-			change_addr = addr
-		}
-		pay_cmd += " -change " + change_addr.String()
-
-		if totalinput > spentsofar {
-			// Add change output
-			outs, er := btc.NewSpendOutputs(change_addr, totalinput-spentsofar, common.CFG.Testnet)
-			if er != nil {
-				err = er.Error()
-				goto error
-			}
-			tx.TxOut = append(tx.TxOut, outs...)
-		}
-
-		buf := new(bytes.Buffer)
+        buf := new(bytes.Buffer)
 		zi := zip.NewWriter(buf)
-
 		was_tx := make(map[[32]byte]bool, len(thisbal))
 		for i := range thisbal {
 			if was_tx[thisbal[i].TxPrevOut.Hash] {
@@ -406,12 +276,10 @@ func dl_payment(w http.ResponseWriter, r *http.Request) {
 				println(er.Error())
 			}
 		}
-
 		fz, _ := zi.Create("balance/unspent.txt")
 		for i := range thisbal {
 			fmt.Fprintln(fz, thisbal[i].UnspentTextLine())
 		}
-
 		if pay_cmd != "" {
 			fz, _ = zi.Create(common.CFG.WebUI.PayCmdName)
 			fz.Write([]byte(pay_cmd))
